52589df133b696ee52245325c180f0f8665d2caa
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-10-03 09:36:33 -0500

    Added Armor.psm1

---
+++ Armor/Armor.psm1
@@ -0,0 +1,19 @@
+#Get public and private function definition files.
+$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+
+#Dot source the files
+ForEach ( $import In @( $Public + $Private ) )
+{
+    Try
+    {
+        . $import.FullName
+    }
+    Catch
+    {
+        Write-Error -Message ( 'Failed to import function {0}: {1}.' -f $import.FullName, $_ )
+    }
+}
+
+# Export the Public modules
+Export-ModuleMember -Function $Public.BaseName
