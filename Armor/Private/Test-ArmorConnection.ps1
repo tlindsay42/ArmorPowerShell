e99cc959b30bb90aa83b92f262fabf47bf663898
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-10-03 09:34:03 -0500

    Added comment-based help

--- Armor/Private/Test-ArmorConnection.ps1
+++ Armor/Private/Test-ArmorConnection.ps1
@@ -1,18 +1,38 @@
 Function Test-ArmorConnection
 {
-	# Test to see if a session has been established with the Armor API
-	# If no token is found, this will throw an error and halt the script
-	# Otherwise, the token is loaded into the script's $Header variable
+	<#
+		.SYNOPSIS
+		Armor API session test.
 
-	Write-Verbose -Message 'Validate that the Armor token exists.'
-	
-	If ( -not $global:ArmorConnection.Token )
+		.DESCRIPTION
+		Test to see if a session has been established with the Armor API.
+		If no token is found, this will throw an error and halt the script.
+		Otherwise, the token is loaded into the script's $Header variable.
+
+		.NOTES
+		Written by Troy Lindsay
+		Twitter: @troylindsay42
+		GitHub: tlindsay42
+
+		.LINK
+		https://github.com/tlindsay42/ArmorPowerShell
+
+		.EXAMPLE
+		Test-ArmorConnection
+	#>
+
+	Process
 	{
-		Write-Warning -Message 'Please connect to only one Armor API session before running this command.'
-		Throw 'A single connection with Connect-Armor is required.'
-	}
+		Write-Verbose -Message 'Validate that the Armor token exists.'
+		
+		If ( -not $global:ArmorConnection.Token )
+		{
+			Write-Warning -Message 'Please connect to only one Armor API session before running this command.'
+			Throw 'A single connection with Connect-Armor is required.'
+		}
 
-	Write-Verbose -Message 'Found an Armor token for authentication.'
+		Write-Verbose -Message 'Found an Armor token for authentication.'
 
-	$script:Header = $global:ArmorConnection.Header
-}
+		$script:Header = $global:ArmorConnection.Header
+	} # End of Process
+} # End of Function
