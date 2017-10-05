deb784ee734bc09d39b6eaec5d7c71924e5a19b7
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-10-05 10:17:46 -0500

    Added .\Tests\build.ps1

---
+++ Tests/build.ps1
@@ -0,0 +1,91 @@
+# Line break for readability in AppVeyor console
+Write-Host -Object ''
+
+# Make sure we're using the Master branch and that it's not a pull request
+# Environmental Variables Guide: https://www.appveyor.com/docs/environment-variables/
+If ( $env:APPVEYOR_REPO_BRANCH -ne 'master' )
+{
+	Write-Warning -Message ( 'Skipping version increment and publish for branch {0}.' -f $env:APPVEYOR_REPO_BRANCH )
+}
+ElseIf ( $env:APPVEYOR_PULL_REQUEST_NUMBER -gt 0 )
+{
+	Write-Warning -Message ( 'Skipping version increment and publish for pull request #{0}' -f $env:APPVEYOR_PULL_REQUEST_NUMBER )
+}
+Else
+{
+	# We're going to add 1 to the revision value since a new commit has been merged to Master
+	# This means that the major / minor / build values will be consistent across GitHub and the Gallery
+	Try
+	{
+		# This is where the module manifest lives
+		$manifestPath = '.\Armor\Armor.psd1'
+
+		# Start by importing the manifest to determine the version, then add 1 to the revision
+		$manifest = Test-ModuleManifest -Path $manifestPath
+		[System.Version] $version = $manifest.Version
+		Write-Host -Object ( 'Old Version: {0}' -f $version )
+
+		[String] $newVersion = New-Object -TypeName 'System.Version' -ArgumentList ( $version.Major, $version.Minor, $version.Build, ( $version.Revision + 1 ) )
+		Write-Host -Object ( 'New Version: {0}' -f $newVersion )
+
+		# Update the manifest with the new version value and fix the weird string replace bug
+		$functionList = ( Get-ChildItem -Path .\Armor\Public ).BaseName
+
+		Update-ModuleManifest -Path $manifestPath -ModuleVersion $newVersion -FunctionsToExport $functionList
+
+		( Get-Content -Path $manifestPath ) -replace 'PSGet_Armor', 'Armor' |
+			ForEach-Object -Process { $_ -replace 'NewManifest', 'Armor' } |
+			ForEach-Object -Process { $_ -replace 'FunctionsToExport = ', 'FunctionsToExport = @(' } |
+			ForEach-Object -Process { $_ -replace ( "{0}'" -f $functionList[-1] ), ( "{0}'')" -f $functionList[-1] ) } |
+			Set-Content -Path $manifestPath
+	}
+	Catch
+	{
+		Throw $_
+	}
+
+	# Update the docs
+	Write-Host -Object 'Building new documentation.' -ForegroundColor 'Yellow'
+	. .\Docs\BuildDocs.ps1
+	Write-Host -Object ''
+
+	# Publish the new version to the PowerShell Gallery
+	Try
+	{
+		Publish-Module -Path '.\Armor' -NuGetApiKey $env:NuGetApiKey -ErrorAction 'Stop'
+		Write-Host -Object ( 'Armor PowerShell Module version {0} published to the PowerShell Gallery.' -f $newVersion ) -ForegroundColor 'Cyan'
+	}
+	Catch
+	{
+		# Sad panda; it broke
+		Write-Warning -Message ( 'Failed to publish update {0} to the PowerShell Gallery.' -f $newVersion )
+		Throw $_
+	}
+
+	# Publish the new version back to Master on GitHub
+	Try
+	{
+		# Set up a path to the git.exe cmd, import posh-git to give us control over git, and then push changes to GitHub
+		# Note that "update version" is included in the appveyor.yml file's "skip a build" regex to avoid a loop
+		If ( ( '{0}\Git\cmd' -f $env:ProgramFiles ) -notin $env:Path.Split( ';' ).Trim() )
+		{
+			$env:Path += ';{0}\Git\cmd' -f $env:ProgramFiles
+		}
+
+		Import-Module -Name 'Posh-Git' -ErrorAction 'Stop'
+
+		git checkout master
+		git add --all
+		git status
+		git commit -s -m ( 'Update version to {0}' -f $newVersion )
+		git push origin master
+
+		Write-Host -Object ( 'Armor PowerShell Module version {0} published to GitHub.' -f $newVersion ) -ForegroundColor 'Cyan'
+	}
+	Catch
+	{
+		# Sad panda; it broke
+		Write-Warning -Message ( 'Publishing update {0} to GitHub failed.' -f $newVersion )
+		throw $_
+	}
+}
