ba006c24d1babe85ae1dd8061683467d16c0f82f
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-10-22 16:53:13 -0500

    Add cmdlet parameter specifications

--- tests/travis-build.ps1
+++ tests/travis-build.ps1
@@ -1,7 +1,7 @@
 # Install InvokeBuild
-Install-Module InvokeBuild -Scope CurrentUser -Force
-Install-Module PlatyPS -Scope CurrentUser -Force
+Install-Module -Name InvokeBuild -Scope CurrentUser -Force
+Install-Module -Name PlatyPS -Scope CurrentUser -Force
 
 # Build the code and perform tests
-Import-module InvokeBuild
+Import-Module -Name InvokeBuild
 Invoke-Build
