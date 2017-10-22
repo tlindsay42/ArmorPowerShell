283d273f6fe6353fc84c507d7ffaf27aee1f7cce
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-10-22 03:09:03 -0500

    Added Restart-ArmorVM.ps1

--- Armor/Private/Get-ArmorApiData.ps1
+++ Armor/Private/Get-ArmorApiData.ps1
@@ -204,6 +204,20 @@ Function Get-ArmorApiData
 					'SuccessCode' = '200'
 				}
 			}
+			'Restart-ArmorVM'      = @{
+				'v1.0' = @{
+					'Description' = 'Reboot the specified virtual machine in your account'
+					'URI'         = @(
+						'/vms/{id}/power/reboot'
+					)
+					'Method'      = 'Post'
+					'Body'        = @{}
+					'Query'       = @{}
+					'Location'    = ''
+					'Filter'      = @{}
+					'SuccessCode' = '200'
+				}
+			}
 			'Start-ArmorVM'        = @{
 				'v1.0' = @{
 					'Description' = 'Power on the specified virtual machine in your account'
