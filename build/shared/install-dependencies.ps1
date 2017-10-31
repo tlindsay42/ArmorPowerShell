97ebbe1103dcac22b89f47aff3dd243158aa7add
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-10-31 00:59:52 -0500

    Add status notifications

--- build/shared/install-dependencies.ps1
+++ build/shared/install-dependencies.ps1
@@ -1,6 +1,9 @@
-Install-PackageProvider -Name NuGet -Force | 
+Write-Host -Object "`nInstalling package provider: 'NuGet'." -ForegroundColor 'Yellow'
+Install-PackageProvider -Name 'NuGet' -Force | 
 	Out-Null
 
-Install-Module -Name Pester -Force
+Write-Host -Object "`nInstalling module: 'Pester'." -ForegroundColor 'Yellow'
+Install-Module -Name 'Pester' -Force
 
-Install-Module -Name posh-git -Force
+Write-Host -Object "`nInstalling module: 'posh-git'." -ForegroundColor 'Yellow'
+Install-Module -Name 'posh-git' -Force
