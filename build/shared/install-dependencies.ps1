ba18f18dc02a9e1e7fd14df3204141149f0ae1cf
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-10-31 22:07:23 -0500

    Update logic for installing package providers and modules in a repeatable manner

--- build/shared/install-dependencies.ps1
+++ build/shared/install-dependencies.ps1
@@ -1,9 +1,18 @@
+ForEach ( $packageProvider In 'NuGet' )
+{
+	Write-Host -Object ( "`nInstalling package provider: '{0}'." -f $packageProvider ) -ForegroundColor 'Yellow'
+	Install-PackageProvider -Name $packageProvider -Force
+}
+Remove-Variable -Name packageProvider
+
 Write-Host -Object "`nInstalling package provider: 'NuGet'." -ForegroundColor 'Yellow'
 Install-PackageProvider -Name 'NuGet' -Force | 
 	Out-Null
 
-Write-Host -Object "`nInstalling module: 'Pester'." -ForegroundColor 'Yellow'
-Install-Module -Name 'Pester' -Force
+ForEach ( $module In 'Pester', 'Posh-Git' )
+{
+	Write-Host -Object ( "`nInstalling module: '{0}'." -f $module ) -ForegroundColor 'Yellow'
+	Install-Module -Name $module -Force
+}
+Remove-Variable -Name module
 
-Write-Host -Object "`nInstalling module: 'posh-git'." -ForegroundColor 'Yellow'
-Install-Module -Name 'posh-git' -Force
