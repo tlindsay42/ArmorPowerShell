4dbf2abd1d99efb4acf59c7263dff36c83a5a0e5
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-11 16:48:48 -0600

    Add Lib directory content import

--- Armor/Armor.psm1
+++ Armor/Armor.psm1
@@ -1,18 +1,19 @@
-#Get public and private function definition files.
-$Public = @( Get-ChildItem -Path ( '{0}\Public\*.ps1' -f $PSScriptRoot ) -ErrorAction 'SilentlyContinue' )
-$Private = @( Get-ChildItem -Path ( '{0}\Private\*.ps1' -f $PSScriptRoot ) -ErrorAction 'SilentlyContinue' )
+# Get class definition files, as well as the private and public function definition files.
+$lib = @( Get-ChildItem -Path ( '{0}\Lib\*.ps1' -f $PSScriptRoot ) -ErrorAction 'SilentlyContinue' )
+$private = @( Get-ChildItem -Path ( '{0}\Private\*.ps1' -f $PSScriptRoot ) -ErrorAction 'SilentlyContinue' )
+$public = @( Get-ChildItem -Path ( '{0}\Public\*.ps1' -f $PSScriptRoot ) -ErrorAction 'SilentlyContinue' )
 
-#Dot source the files
-ForEach ( $import In @( $Public + $Private ) )
+# Source the definition files
+ForEach ( $import In @( $lib + $private + $public ) )
 {
-    Try
-    {
-        . $import.FullName
-    }
-    Catch
-    {
-        Write-Error -Message ( 'Failed to import function {0}: {1}.' -f $import.FullName, $_ )
-    }
+	Try
+	{
+		. $import.FullName
+	}
+	Catch
+	{
+		Write-Error -Message ( 'Failed to import function {0}: {1}.' -f $import.FullName, $_ )
+	}
 }
 
 # Export the Public modules
