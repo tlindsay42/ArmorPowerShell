4715b9069ade8408ba2427f8fd9c0684028ed3d9
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-12 17:13:32 -0600

    Add 12-ArmorDepartment.ps1

---
+++ Armor/Lib/12-ArmorDepartment.ps1
@@ -0,0 +1,14 @@
+Class ArmorDepartment
+{
+	[ValidateRange( 1, 65535 )]
+	[UInt16] $ID
+
+	[ValidateNotNullOrEmpty()]
+	[String] $Name
+
+	[ValidateRange( 1, 65535 )]
+	[UInt16] $Account
+
+	#Constructors
+	ArmorDepartment () {}
+}
