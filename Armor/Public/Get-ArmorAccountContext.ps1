0ac7b7ea7eccd1c7173667d19c41f86400e01cba
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-12 18:41:09 -0600

    Add Get-ArmorAccountContext.ps1

---
+++ Armor/Public/Get-ArmorAccountContext.ps1
@@ -0,0 +1,56 @@
+Function Get-ArmorAccountContext
+{
+	<#
+		.SYNOPSIS
+		{ required: high level overview }
+
+		.DESCRIPTION
+		{ required: more detailed description of the function's purpose }
+
+		.NOTES
+		Troy Lindsay
+		Twitter: @troylindsay42
+		GitHub: tlindsay42
+
+		.PARAMETER Parameter
+		{ required: description of the specified input parameter's purpose }
+
+		.INPUTS
+		{ required: .NET Framework object types that can be piped in and a description of the input objects }
+
+		.OUTPUTS
+		{ required: .NET Framework object types that the cmdlet returns and a description of the returned objects }
+
+		.LINK
+		https://github.com/tlindsay42/ArmorPowerShell
+
+		.LINK
+		https://docs.armor.com/display/KBSS/Armor+API+Guide
+
+		.LINK
+		https://developer.armor.com/
+
+		.EXAMPLE
+		{required: show one or more examples using the function}
+	#>
+
+	[CmdletBinding()]
+	Param ()
+
+	Begin
+	{
+		$function = $MyInvocation.MyCommand.Name
+
+		Write-Verbose -Message ( 'Beginning {0}.' -f $function )
+	} # End of Begin
+
+	Process
+	{
+		Return $Global:ArmorSession.GetAccountContext()
+	} # End of Process
+
+	End
+	{
+		Write-Verbose -Message ( 'Ending {0}.' -f $function )
+	} # End of End
+} # End of Function
