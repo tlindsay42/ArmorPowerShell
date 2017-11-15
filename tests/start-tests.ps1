46a16cd55a833b0a3d61610e2a59fa36d767c5b3
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-15 08:46:52 -0600

    Revert slashes in paths to backslashes

--- tests/start-tests.ps1
+++ tests/start-tests.ps1
@@ -3,11 +3,11 @@ ElseIf ( $env:TRAVIS -eq $true ) { $ciName = 'Travis' }
 Else { Throw 'Unknown continuous integration environment.' }
 
 Write-Host -Object "`nInvoking Pester test framework." -ForegroundColor 'Yellow'
-$testsResults = Invoke-Pester -Path './tests' `
+$testsResults = Invoke-Pester -Path '.\tests' `
 	-OutputFormat NUnitXml `
-	-OutputFile ( './tests/results/{0}TestsResults.xml' -f $ciName ) `
-	-CodeCoverage ( Get-ChildItem -Path './Armor' -Include '*.ps1' -Recurse ) `
-	-CodeCoverageOutputFile ( './tests/results/{0}CodeCoverageResults.xml' -f $ciName ) `
+	-OutputFile ( '.\tests\results\{0}TestsResults.xml' -f $ciName ) `
+	-CodeCoverage ( Get-ChildItem -Path '.\Armor' -Include '*.ps1' -Recurse ) `
+	-CodeCoverageOutputFile ( '.\tests\results\{0}CodeCoverageResults.xml' -f $ciName ) `
 	-CodeCoverageOutputFileFormat 'JaCoCo' `
 	-PassThru
 
@@ -15,7 +15,7 @@ If ( $ciName -eq 'AppVeyor' )
 {
 	( New-Object -TypeName 'System.Net.WebClient' ).UploadFile( 
 		( 'https://ci.appveyor.com/api/testresults/nunit/{0}' -f $env:APPVEYOR_JOB_ID ), 
-		( Resolve-Path -Path './tests/results/AppVeyorTestsResults.xml' )
+		( Resolve-Path -Path '.\tests\results\AppVeyorTestsResults.xml' )
 	)
 }
 
