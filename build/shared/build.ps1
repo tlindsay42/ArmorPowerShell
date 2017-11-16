b3b66afbf14cd6f4de76f16cf5241311e55d61b4
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-16 09:37:39 -0600

    Add code coverage badges

--- build/shared/build.ps1
+++ build/shared/build.ps1
@@ -88,7 +88,9 @@ Write-Host -Object "`nBuilding the documentation." -ForegroundColor 'Yellow'
 $content = @()
 $content += "# Armor PowerShell Module
 
-**Windows PowerShell** [![Build status](https://ci.appveyor.com/api/projects/status/x4ik2enxvdc5h0x6/branch/master?svg=true)](https://ci.appveyor.com/project/tlindsay42/armorpowershell/branch/master) **PowerShell Core** [![Build status](https://travis-ci.org/tlindsay42/ArmorPowerShell.svg?branch=master)](https://travis-ci.org/tlindsay42/ArmorPowerShell) **Documentation** [![Documentation Status](https://readthedocs.org/projects/armorpowershell/badge/?version=latest)](http://armorpowershell.readthedocs.io/en/latest/?badge=latest) **Install** [![PS Gallery](https://img.shields.io/badge/install-PS%20Gallery-blue.svg)](https://www.powershellgallery.com/packages/Armor)
+| **Windows PowerShell** | **PowerShell Core** | **Code Coverage** | **Documentation** | **Install** |
+| :--------------------: | :-----------------: | :---------------: | :---------------: | :---------: |
+| [![Build status](https://ci.appveyor.com/api/projects/status/x4ik2enxvdc5h0x6/branch/master?svg=true)](https://ci.appveyor.com/project/tlindsay42/armorpowershell/branch/master) | [![Build status](https://travis-ci.org/tlindsay42/ArmorPowerShell.svg?branch=master)](https://travis-ci.org/tlindsay42/ArmorPowerShell) | [![Coverage Status](https://coveralls.io/repos/github/tlindsay42/ArmorPowerShell/badge.svg?branch=master)](https://coveralls.io/github/tlindsay42/ArmorPowerShell?branch=master) | [![Documentation Status](https://readthedocs.org/projects/armorpowershell/badge/?version=latest)](http://armorpowershell.readthedocs.io/en/latest/?badge=latest) | [![PS Gallery](https://img.shields.io/badge/install-PS%20Gallery-blue.svg)](https://www.powershellgallery.com/packages/Armor)
 
 This is a community project that provides a powerful command-line interface for managing and monitoring your **[Armor Complete](https://www.armor.com/armor-complete-secure-hosting/ 'Armor Complete Product Page')** (secure public cloud) and **[Armor Anywhere](https://www.armor.com/armor-anywhere-security/ 'Armor Anywhere Product Page')** (security as a service) environments & accounts via a PowerShell module that interfaces with the published [RESTful APIs](https://docs.armor.com/display/KBSS/Armor+API+Guide 'Armor API Guide').  It is continuously tested on Windows via [AppVeyor](https://ci.appveyor.com/project/tlindsay42/ArmorPowerShell), as well as on Ubuntu Linux and macOS via [Travis CI](https://travis-ci.org/tlindsay42/ArmorPowerShell), and it is published on the [PowerShell Gallery](https://www.powershellgallery.com/packages/Armor).
 
@@ -106,6 +108,11 @@ $content += 'Welcome to the Armor PowerShell Module
 .. image:: https://travis-ci.org/tlindsay42/ArmorPowerShell.svg?branch=master
    :target: https://travis-ci.org/tlindsay42/ArmorPowerShell
    :alt: PowerShell Core Status
+
+
+.. image:: https://coveralls.io/repos/github/tlindsay42/ArmorPowerShell/badge.svg?branch=master
+   :target: https://coveralls.io/github/tlindsay42/ArmorPowerShell?branch=master
+   :alt: Code Coverage Status
 
 .. image:: https://readthedocs.org/projects/armorpowershell/badge/?version=latest
    :target: http://armorpowershell.readthedocs.io/en/latest/?badge=latest
