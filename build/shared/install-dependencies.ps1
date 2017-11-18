248add178a6da8d702deca5eb626dee9d89a3249
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-17 22:16:20 -0600

    Prune old code

--- build/shared/install-dependencies.ps1
+++ build/shared/install-dependencies.ps1
@@ -1,18 +1,8 @@
-Get-PackageProvider
-
-# ForEach ( $packageProvider In 'NuGet' )
-# {
-# 	Write-Host -Object ( "`nInstalling package provider: '{0}'." -f $packageProvider ) -ForegroundColor 'Yellow'
-# 	Install-PackageProvider -Name $packageProvider -Force |
-# 		Out-Null
-# }
-# Remove-Variable -Name packageProvider
-
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy 'Trusted'
 
 ForEach ( $module In 'Pester', 'Posh-Git', 'Coveralls' )
 {
-	Write-Host -Object ( "`nInstalling module: '{0}'." -f $module ) -ForegroundColor 'Yellow'
+	Write-Host -Object ( "Installing module: '{0}'." -f $module ) -ForegroundColor 'Yellow'
 	Install-Module -Name $module -Force |
 		Out-Null
 }
