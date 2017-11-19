da5a2a118b3b0e8cf5a63b55858198eaed34e8f1
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-19 03:45:33 -0600

    Fix quotes

--- build/appveyor/publish-psgallery.ps1
+++ build/appveyor/publish-psgallery.ps1
@@ -10,5 +10,5 @@ ElseIf ( $env:APPVEYOR_JOB_NUMBER -eq 1 )
 {
 	# Publish the new version to the PowerShell Gallery
 	Publish-Module -Path $env:MODULE_PATH -NuGetApiKey $env:NUGET_API_KEY -ErrorAction 'Stop'
-	Write-Host -Object ( '`n{0} Module version {1} published to the PowerShell Gallery.`n' -f $env:APPVEYOR_PROJECT_NAME, $env:MODULE_VERSION ) -ForegroundColor 'Yellow'
+	Write-Host -Object ( "`n{0} Module version {1} published to the PowerShell Gallery.`n" -f $env:APPVEYOR_PROJECT_NAME, $env:MODULE_VERSION ) -ForegroundColor 'Yellow'
 }
