dd7f96f09433b13a70bc2fc4ee2a9b2c4310947a
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-19 01:43:29 -0600

    Standardized environment variable naming convention and usage

--- build/appveyor/publish-psgallery.ps1
+++ build/appveyor/publish-psgallery.ps1
@@ -11,12 +11,12 @@ Else
 	# Publish the new version to the PowerShell Gallery
 	Try
 	{
-		Publish-Module -Path '.\Armor' -NuGetApiKey $env:NuGetApiKey -ErrorAction 'Stop'
-		Write-Host -Object ( 'Armor PowerShell Module version {0} published to the PowerShell Gallery.' -f $newVersion ) -ForegroundColor 'Cyan'
+		Publish-Module -Path $env:MODULE_PATH -NuGetApiKey $env:NUGET_API_KEY -ErrorAction 'Stop'
+		Write-Host -Object ( '`n{0} Module version {1} published to the PowerShell Gallery.`n' -f $env:APPVEYOR_PROJECT_NAME, $env:MODULE_VERSION ) -ForegroundColor 'Yellow'
 	}
 	Catch
 	{
-		Write-Warning -Message ( 'Failed to publish update {0} to the PowerShell Gallery.' -f $newVersion )
+		Write-Warning -Message ( 'Failed to publish update {0} to the PowerShell Gallery.' -f $env:MODULE_VERSION )
 		Throw $_
 	}
 }
--- tests/start-tests.ps1
+++ tests/start-tests.ps1
@@ -3,11 +3,11 @@ ElseIf ( $env:TRAVIS -eq $true ) { $ciName = 'TravisCI' }
 Else { Throw 'Unknown continuous integration environment.' }
 
 Write-Host -Object "`nInvoking Pester test framework." -ForegroundColor 'Yellow'
-$testsResults = Invoke-Pester -Path '.\tests' `
+$testsResults = Invoke-Pester -Path ( '{0}\tests' -f $env:BUILD_PATH ) `
 	-OutputFormat NUnitXml `
-	-OutputFile ( '.\tests\results\{0}TestsResults.xml' -f $ciName ) `
-	-CodeCoverage ( Get-ChildItem -Path '.\Armor' -Include '*.ps1' -Recurse ) `
-	-CodeCoverageOutputFile ( '.\tests\results\{0}CodeCoverageResults.xml' -f $ciName ) `
+	-OutputFile ( '{0}\tests\results\{1}TestsResults.xml' -f $env:BUILD_PATH, $ciName ) `
+	-CodeCoverage ( Get-ChildItem -Path $env:MODULE_PATH -Include '*.ps1' -Recurse ) `
+	-CodeCoverageOutputFile ( '{0}\tests\results\{1}CodeCoverageResults.xml' -f $env:BUILD_PATH, $ciName ) `
 	-CodeCoverageOutputFileFormat 'JaCoCo' `
 	-PassThru
 
@@ -15,10 +15,10 @@ If ( $ciName -eq 'AppVeyor' )
 {
 	( New-Object -TypeName 'System.Net.WebClient' ).UploadFile( 
 		( 'https://ci.appveyor.com/api/testresults/nunit/{0}' -f $env:APPVEYOR_JOB_ID ), 
-		( Resolve-Path -Path '.\tests\results\AppVeyorTestsResults.xml' )
+		( '{0}\tests\results\AppVeyorTestsResults.xml' -f $env:BUILD_PATH )
 	)
 
-	$coverage = Format-Coverage -PesterResults $testsResults -CoverallsApiToken $env:CoverallsApiKey -BranchName $env:APPVEYOR_REPO_BRANCH
+	$coverage = Format-Coverage -PesterResults $testsResults -CoverallsApiToken $env:COVERALLS_API_KEY -BranchName $env:APPVEYOR_REPO_BRANCH
 
 	Publish-Coverage -Coverage $coverage
 }
