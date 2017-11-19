a28b20e49857ef6a746493086f16ff08b2447b35
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-19 03:09:56 -0600

    Remove try / catch

--- build/appveyor/publish-psgallery.ps1
+++ build/appveyor/publish-psgallery.ps1
@@ -9,14 +9,6 @@ ElseIf ( $env:APPVEYOR_PULL_REQUEST_NUMBER -gt 0 )
 Else
 {
 	# Publish the new version to the PowerShell Gallery
-	Try
-	{
-		Publish-Module -Path $env:MODULE_PATH -NuGetApiKey $env:NUGET_API_KEY -ErrorAction 'Stop'
-		Write-Host -Object ( '`n{0} Module version {1} published to the PowerShell Gallery.`n' -f $env:APPVEYOR_PROJECT_NAME, $env:MODULE_VERSION ) -ForegroundColor 'Yellow'
-	}
-	Catch
-	{
-		Write-Warning -Message ( 'Failed to publish update {0} to the PowerShell Gallery.' -f $env:MODULE_VERSION )
-		Throw $_
-	}
+	Publish-Module -Path $env:MODULE_PATH -NuGetApiKey $env:NUGET_API_KEY -ErrorAction 'Stop'
+	Write-Host -Object ( '`n{0} Module version {1} published to the PowerShell Gallery.`n' -f $env:APPVEYOR_PROJECT_NAME, $env:MODULE_VERSION ) -ForegroundColor 'Yellow'
 }
