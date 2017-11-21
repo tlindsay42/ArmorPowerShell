e71ec7477de1796b94a74a4bfa9bc8cb6dc5fcb1
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-21 03:09:48 -0600

    Fix test result pathing

--- tests/start-tests.ps1
+++ tests/start-tests.ps1
@@ -1,3 +1,5 @@
+$testsPath = '{0}\tests' -f $env:BUILD_PATH
+
 if ( $env:APPVEYOR -eq $true ) {
     $ciName = 'AppVeyor' 
 }
@@ -8,17 +10,23 @@ else {
     throw 'Unknown continuous integration environment.'
 }
 
+$testsResultsPath = New-Item -Path $testsPath -Name 'results' -ItemType 'Directory' -ErrorAction 'Stop'
+
+if ( -not ( Test-Path -Path $testsPath ) ) {
+    throw ( 'Directory not found: "{0}".' -f $testsPath )
+}
+
 Write-Host -Object "`nInvoking Pester test framework." -ForegroundColor 'Yellow'
-$testsResults = Invoke-Pester -Path ( '{0}\tests' -f $env:BUILD_PATH ) `
+$testsResults = Invoke-Pester -Path $testsPath `
     -OutputFormat 'NUnitXml' `
-    -OutputFile ( '{0}\tests\results\{1}TestsResults.xml' -f $env:BUILD_PATH, $ciName ) `
+    -OutputFile ( '{0}\{1}TestsResults.xml' -f $testsResultsPath, $ciName ) `
     -CodeCoverage ( Get-ChildItem -Path $env:MODULE_PATH -Include '*.ps1' -Recurse ) `
-    -CodeCoverageOutputFile ( '{0}\tests\results\{1}CodeCoverageResults.xml' -f $env:BUILD_PATH, $ciName ) `
+    -CodeCoverageOutputFile ( '{0}\{1}CodeCoverageResults.xml' -f $testsResultsPath, $ciName ) `
     -CodeCoverageOutputFileFormat 'JaCoCo' `
     -PassThru
 
 if ( $ciName -eq 'AppVeyor' ) {
-    ( New-Object -TypeName 'System.Net.WebClient' ).UploadFile( 
+    ( New-Object -TypeName 'System.Net.WebClient' ).UploadFile(
         ( 'https://ci.appveyor.com/api/testresults/nunit/{0}' -f $env:APPVEYOR_JOB_ID ), 
         ( '{0}\tests\results\AppVeyorTestsResults.xml' -f $env:BUILD_PATH )
     )
