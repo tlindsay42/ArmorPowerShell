3f7a74cc0c8324de26e5ec429ac7a9d257086340
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-21 23:27:43 -0600

    Fix test result directory handling

--- tests/start-tests.ps1
+++ tests/start-tests.ps1
@@ -1,33 +1,46 @@
 $testsPath = '{0}\tests' -f $env:BUILD_PATH
+$results = 'results'
+$testsResultsPath = '{0}\{1}' -f $testsPath, $results
+$testsResultsDirectory = $null
+$testsResultsFilePath = '{0}\{1}TestsResults.xml' -f $testsResultsPath, $ciName
+$codeCoverageResultsFilePath = $null
 
 if ( $env:APPVEYOR -eq $true ) {
-    $ciName = 'AppVeyor' 
+    $ciName = 'AppVeyor'
 }
 elseif ( $env:TRAVIS -eq $true ) {
-    $ciName = 'TravisCI' 
+    $ciName = 'TravisCI'
 }
 else {
     throw 'Unknown continuous integration environment.'
 }
 
-$testsResultsPath = New-Item -Path $testsPath -Name 'results' -ItemType 'Directory' -ErrorAction 'Stop'
+$codeCoverageResultsFilePath = '{0}\{1}CodeCoverageResults.xml' -f $testsResultsPath, $ciName
 
-if ( -not ( Test-Path -Path $testsPath ) ) {
-    throw ( 'Directory not found: "{0}".' -f $testsPath )
+if ( -not ( Test-Path -Path $testsResultsPath ) ) {
+    $testsResultsDirectory = New-Item -Path $testsPath -Name $results -ItemType 'Directory' -Force -ErrorAction 'Stop'
+
+    if ( -not $testsResultsDirectory ) {
+        throw ( 'Directory not found: "{0}".' -f $testsResultsDirectory )
+    }
 }
 
 Write-Host -Object "`nInvoking Pester test framework." -ForegroundColor 'Yellow'
 $testsResults = Invoke-Pester -Path $testsPath `
     -OutputFormat 'NUnitXml' `
-    -OutputFile ( '{0}\{1}TestsResults.xml' -f $testsResultsPath, $ciName ) `
+    -OutputFile $testsResultsFilePath `
     -CodeCoverage ( Get-ChildItem -Path $env:MODULE_PATH -Include '*.ps1' -Recurse ) `
-    -CodeCoverageOutputFile ( '{0}\{1}CodeCoverageResults.xml' -f $testsResultsPath, $ciName ) `
+    -CodeCoverageOutputFile $codeCoverageResultsFilePath `
     -CodeCoverageOutputFileFormat 'JaCoCo' `
     -PassThru
 
+if ( -not ( Test-Path -Path $testsResultsFilePath ) ) {
+    throw ( 'File not found: "{0}".' -f $testsResultsFilePath )
+}
+
 if ( $ciName -eq 'AppVeyor' ) {
     ( New-Object -TypeName 'System.Net.WebClient' ).UploadFile(
-        ( 'https://ci.appveyor.com/api/testresults/nunit/{0}' -f $env:APPVEYOR_JOB_ID ), 
+        ( 'https://ci.appveyor.com/api/testresults/nunit/{0}' -f $env:APPVEYOR_JOB_ID ),
         ( '{0}\tests\results\AppVeyorTestsResults.xml' -f $env:BUILD_PATH )
     )
 
