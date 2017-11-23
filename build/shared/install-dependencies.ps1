2451d8c2a1cb31fa2d7dbfd302a43b4179f925c5
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-23 04:38:57 -0600

    Fix module path definition

--- build/shared/install-dependencies.ps1
+++ build/shared/install-dependencies.ps1
@@ -1,10 +1,10 @@
-# if ( $env:APPVEYOR -eq $true ) {
+if ( $env:APPVEYOR -eq $true ) {
 #     $env:BUILD_PATH = $env:APPVEYOR_BUILD_FOLDER
-#     $env:MODULE_PATH = '{0}\{1}' -f $env:BUILD_PATH, $env:MODULE_NAME
+    $env:MODULE_PATH = '{0}\{1}' -f $env:BUILD_PATH, $env:MODULE_NAME
 #     $env:MODULE_VERSION = $env:APPVEYOR_BUILD_VERSION
 #     $env:OWNER_NAME = $env:APPVEYOR_ACCOUNT_NAME
 #     $env:PROJECT_NAME = $env:APPVEYOR_PROJECT_NAME
-# }
+}
 
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy 'Trusted'
 
