4b419bbdd4894add6fc175c70ad92122ed5967b2
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-27 11:49:22 -0600

    Fix W2012 Install-Module

--- build/shared/install-dependencies.ps1
+++ build/shared/install-dependencies.ps1
@@ -4,8 +4,14 @@ Write-Host -Object "`nInstalling modules:" -ForegroundColor 'Yellow'
 
 foreach ( $moduleName in 'Pester', 'Coveralls' ) {
     Write-Host -Object ( '   {0}' -f $moduleName )
-    Install-Module -Name $moduleName -Scope 'CurrentUser' -SkipPublisherCheck -Force -Confirm:$false |
-        Out-Null
+    if ( $env:APPVEYOR_BUILD_WORKER_IMAGE -eq 'Visual Studio 2015' ) {
+        Install-Module -Name $moduleName -Scope 'CurrentUser' -Force -Confirm:$false |
+            Out-Null
+    }
+    else {
+        Install-Module -Name $moduleName -Scope 'CurrentUser' -SkipPublisherCheck -Force -Confirm:$false |
+            Out-Null
+    }
 }
 Remove-Variable -Name moduleName
 
