88fe7ce561f1e88c4d9d6d500fcce5e8f3f23592
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-26 22:40:26 -0600

    Move AppVeyor build-specific environment variables to init

--- build/shared/install-dependencies.ps1
+++ build/shared/install-dependencies.ps1
@@ -1,17 +1,9 @@
-if ( $env:APPVEYOR -eq $true ) {
-    $env:BUILD_PATH = $env:APPVEYOR_BUILD_FOLDER
-    $env:MODULE_PATH = '{0}\{1}' -f $env:BUILD_PATH, $env:MODULE_NAME
-    $env:MODULE_VERSION = ( $env:APPVEYOR_BUILD_VERSION ).Split( '-' )[0]
-    $env:OWNER_NAME = $env:APPVEYOR_ACCOUNT_NAME
-    $env:PROJECT_NAME = $env:APPVEYOR_PROJECT_NAME
-}
-
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy 'Trusted'
 
 Write-Host -Object "`nInstalling modules:" -ForegroundColor 'Yellow'
 
-foreach ( $moduleName in 'Pester', 'Posh-Git', 'Coveralls' ) {
-    Write-Host -Object ( '   {0}' -f $module )
+foreach ( $moduleName in 'Pester', 'Coveralls' ) {
+    Write-Host -Object ( '   {0}' -f $moduleName )
     Install-Module -Name $moduleName -Scope 'CurrentUser' -Force -Confirm:$false |
         Out-Null
 }
