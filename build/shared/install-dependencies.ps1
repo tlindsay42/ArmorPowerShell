e1cd2547ad77d4e2bcccf32e3709e5d3e625b838
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-11-30 08:25:49 -0600

    Set repository when installing modules

--- build/shared/install-dependencies.ps1
+++ build/shared/install-dependencies.ps1
@@ -5,11 +5,11 @@ Write-Host -Object "`nInstalling modules:" -ForegroundColor 'Yellow'
 foreach ( $moduleName in 'Pester', 'Coveralls' ) {
     Write-Host -Object ( '   {0}' -f $moduleName )
     if ( $env:APPVEYOR_BUILD_WORKER_IMAGE -eq 'Visual Studio 2015' ) {
-        Install-Module -Name $moduleName -Scope 'CurrentUser' -Force -Confirm:$false |
+        Install-Module -Name $moduleName -Scope 'CurrentUser' -Repository 'PSGallery' -Force -Confirm:$false |
             Out-Null
     }
     else {
-        Install-Module -Name $moduleName -Scope 'CurrentUser' -SkipPublisherCheck -Force -Confirm:$false |
+        Install-Module -Name $moduleName -Scope 'CurrentUser' -Repository 'PSGallery' -SkipPublisherCheck -Force -Confirm:$false |
             Out-Null
     }
 }
