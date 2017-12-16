721648c12f388183608b0890ef4ab09e6bceee0b
Troy Lindsay <tlindsay42@users.noreply.github.com>
2017-12-15 20:04:47 -0600

    Fix upload of Pester test results

--- tests/start-tests.ps1
+++ tests/start-tests.ps1
@@ -1,26 +1,14 @@
 $testsPath = '{0}\tests' -f $env:CI_BUILD_PATH
 $results = 'results'
 $testsResultsPath = '{0}\{1}' -f $testsPath, $results
+$testsResultsFilePath = '{0}\{1}TestsResults.xml' -f $testsResultsPath, $env:CI_NAME
+$codeCoverageResultsFilePath = '{0}\{1}CodeCoverageResults.xml' -f $testsResultsPath, $env:CI_NAME
 $testsResultsDirectory = $null
-$testsResultsFilePath = '{0}\{1}TestsResults.xml' -f $testsResultsPath, $ciName
-$codeCoverageResultsFilePath = $null
-
-if ( $env:APPVEYOR -eq $true ) {
-    $ciName = 'AppVeyor'
-}
-elseif ( $env:TRAVIS -eq $true ) {
-    $ciName = 'TravisCI'
-}
-else {
-    throw 'Unknown continuous integration environment.'
-}
-
-$codeCoverageResultsFilePath = '{0}\{1}CodeCoverageResults.xml' -f $testsResultsPath, $ciName
 
 if ( -not ( Test-Path -Path $testsResultsPath ) ) {
     $testsResultsDirectory = New-Item -Path $testsPath -Name $results -ItemType 'Directory' -Force -ErrorAction 'Stop'
 
-    if ( -not $testsResultsDirectory ) {
+    if ( -not ( Test-Path -Path $testsResultsPath ) ) {
         throw ( 'Directory not found: "{0}".' -f $testsResultsDirectory )
     }
 }
@@ -38,7 +26,7 @@ if ( -not ( Test-Path -Path $testsResultsFilePath ) ) {
     throw ( 'File not found: "{0}".' -f $testsResultsFilePath )
 }
 
-if ( $ciName -eq 'AppVeyor' ) {
+if ( $env:APPVEYOR -eq $true ) {
     ( New-Object -TypeName 'System.Net.WebClient' ).UploadFile(
         ( 'https://ci.appveyor.com/api/testresults/nunit/{0}' -f $env:APPVEYOR_JOB_ID ),
         ( '{0}\tests\results\AppVeyorTestsResults.xml' -f $env:CI_BUILD_PATH )
