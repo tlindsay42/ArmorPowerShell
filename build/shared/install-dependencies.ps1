85a5a01cb70e652de354b2b4a444f57c6cfce5af
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-01-29 23:34:06 -0600

    Improve dependency installation output

--- build/shared/install-dependencies.ps1
+++ build/shared/install-dependencies.ps1
@@ -4,19 +4,18 @@ Write-Host -Object "`nInstalling package providers:" -ForegroundColor 'Yellow'
 foreach ( $providerName in 'NuGet', 'PowerShellGet' ) {
     if ( -not ( Get-PackageProvider $providerName -ErrorAction 'SilentlyContinue' ) ) {
         Install-PackageProvider -Name $providerName -Scope 'CurrentUser' -Force -ForceBootstrap
-        Get-PackageProvider -Name $providerName
     }
+    Get-PackageProvider -Name $providerName
 }
 
 Write-Host -Object "`nInstalling modules:" -ForegroundColor 'Yellow'
 foreach ( $moduleName in 'PowerShellGet', 'Pester', 'Coveralls' ) {
-    Write-Host -Object ( '   {0}' -f $moduleName )
     if ( $env:APPVEYOR_BUILD_WORKER_IMAGE -eq 'Visual Studio 2015' ) {
-        Install-Module -Name $moduleName -Scope 'CurrentUser' -Repository 'PSGallery' -Force -Confirm:$false |
+        Install-Module -Name $moduleName -Scope 'CurrentUser' -Repository 'PSGallery' -AllowClobber -Force -Confirm:$false |
             Out-Null
     }
     else {
-        Install-Module -Name $moduleName -Scope 'CurrentUser' -Repository 'PSGallery' -SkipPublisherCheck -Force -Confirm:$false |
+        Install-Module -Name $moduleName -Scope 'CurrentUser' -Repository 'PSGallery' -AllowClobber -SkipPublisherCheck -Force -Confirm:$false |
             Out-Null
     }
     
