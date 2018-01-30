0e441637193d526bfce24e3f1a5355bc950cb669
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-01-29 23:16:52 -0600

    Print additional dependency detail

--- build/shared/install-dependencies.ps1
+++ build/shared/install-dependencies.ps1
@@ -1,16 +1,16 @@
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy 'Trusted'
 
-Write-Host -Object "`nInstalling modules:" -ForegroundColor 'Yellow'
-
+Write-Host -Object "`nInstalling package providers:" -ForegroundColor 'Yellow'
 foreach ( $providerName in 'NuGet', 'PowerShellGet' ) {
+    Write-Host -Object ( '   {0}' -f $moduleName )
     if ( -not ( Get-PackageProvider $providerName -ErrorAction 'SilentlyContinue' ) ) {
         Install-PackageProvider -Name $providerName -Scope 'CurrentUser' -Force -ForceBootstrap
+        Get-PackageProvider -Name $providerName
     }
 }
 
-Install-Module -Name 'PowerShellGet' -Repository 'PSGallery' -SkipPublisherCheck -Force -Confirm:$false
-
-foreach ( $moduleName in 'Pester', 'Coveralls' ) {
+Write-Host -Object "`nInstalling modules:" -ForegroundColor 'Yellow'
+foreach ( $moduleName in 'PowerShellGet', 'Pester', 'Coveralls' ) {
     Write-Host -Object ( '   {0}' -f $moduleName )
     if ( $env:APPVEYOR_BUILD_WORKER_IMAGE -eq 'Visual Studio 2015' ) {
         Install-Module -Name $moduleName -Scope 'CurrentUser' -Repository 'PSGallery' -Force -Confirm:$false |
@@ -20,8 +20,9 @@ foreach ( $moduleName in 'Pester', 'Coveralls' ) {
         Install-Module -Name $moduleName -Scope 'CurrentUser' -Repository 'PSGallery' -SkipPublisherCheck -Force -Confirm:$false |
             Out-Null
     }
-
+    
     Import-Module -Name $moduleName
+    Get-Module -Name $moduleName
 }
 Remove-Variable -Name 'moduleName'
 
