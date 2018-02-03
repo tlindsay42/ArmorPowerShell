0b3bef145dc845ffd01ec497573ebca7a8c514b2
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-02-03 05:11:25 -0600

    Refactor pipelining logic

--- Armor/Private/Format-ArmorApiJsonRequestBody.ps1
+++ Armor/Private/Format-ArmorApiJsonRequestBody.ps1
@@ -68,94 +68,94 @@ function Format-ArmorApiJsonRequestBody {
     } # End of begin
 
     process {
-        if ( $resources.Method -eq 'Get' ) {
-            return $null
-        }
+        $return = $null
 
-        # Inventory all invoked parameters
-        $setParameters = $PSCmdlet.MyInvocation.BoundParameters
+        if ( $resources.Method -ne 'Get' ) {
+            # Inventory all invoked parameters
+            $setParameters = $PSCmdlet.MyInvocation.BoundParameters
 
-        Write-Verbose -Message ( 'List of set parameters: {0}.' -f ( $setParameters.Values.Name.Where( { $_ -notin $excludedParameters } ) -join ', ' ) )
+            Write-Verbose -Message ( 'List of set parameters: {0}.' -f ( $setParameters.Values.Name.Where( { $_ -notin $excludedParameters } ) -join ', ' ) )
 
-        Write-Verbose -Message 'Build the body parameters.'
+            Write-Verbose -Message 'Build the body parameters.'
 
-        $bodyString = @{}
+            $bodyString = @{}
 
-        # Walk through all of the available body options presented by the endpoint
-        # Note: Keys are used to search in case the value changes in the future across different API versions
-        foreach ( $bodyKey in $BodyKeys ) {
-            Write-Verbose -Message ( 'Adding {0}...' -f $bodyKey )
+            # Walk through all of the available body options presented by the endpoint
+            # Note: Keys are used to search in case the value changes in the future across different API versions
+            foreach ( $bodyKey in $BodyKeys ) {
+                Write-Verbose -Message ( 'Adding {0}...' -f $bodyKey )
 
-            $bodyKeyNoUnderscore = $bodyKey -replace '_', ''
+                $bodyKeyNoUnderscore = $bodyKey -replace '_', ''
 
-            # Array Object
-            if ( $resources.Body.$bodyKey.GetType().BaseType.Name -eq 'Array' ) {
-                $arraystring = @{}
+                # Array Object
+                if ( $resources.Body.$bodyKey.GetType().BaseType.Name -eq 'Array' ) {
+                    $arraystring = @{}
 
-                foreach ( $arrayItem in $resources.Body.$bodyKey.Keys ) {
+                    foreach ( $arrayItem in $resources.Body.$bodyKey.Keys ) {
+                        # Walk through all of the parameters defined in the function
+                        # Both the parameter name and parameter alias are used to match against a body option
+                        # It is suggested to make the parameter name "human friendly" and set an alias corresponding to the body option name
+                        foreach ( $parameter in $Parameters.Where( { $_.Name -notin $excludedParameters } ) ) {
+                            # if the parameter name or alias matches the body option name, build a body string
+                            if ( $parameter.Name -eq $arrayItem -or $parameter.Aliases -eq $arrayItem) {
+                                # Switch variable types
+                                if ( ( Get-Variable -Name $parameter.Name ).Value.GetType().Name -eq 'SwitchParameter' ) {
+                                    $arraystring.Add( $arrayItem, ( Get-Variable -Name $parameter.Name ).Value.IsPresent )
+                                }
+                                # All other variable types
+                                elseif ( ( Get-Variable -Name $parameter.Name ).Value -ne $null ) {
+                                    $arraystring.Add( $arrayItem, ( Get-Variable -Name $parameter.Name ).Value )
+                                }
+                            }
+                        }
+                    }
+
+                    $bodyString.Add( $bodyKey, @( $arraystring ) )
+                }
+                # Non-Array Object
+                else {
                     # Walk through all of the parameters defined in the function
                     # Both the parameter name and parameter alias are used to match against a body option
                     # It is suggested to make the parameter name "human friendly" and set an alias corresponding to the body option name
                     foreach ( $parameter in $Parameters.Where( { $_.Name -notin $excludedParameters } ) ) {
                         # if the parameter name or alias matches the body option name, build a body string
-                        if ( $parameter.Name -eq $arrayItem -or $parameter.Aliases -eq $arrayItem) {
+                        if (
+                            ( $parameter.Name -eq $bodyKey -or $parameter.Aliases -contains $bodyKey ) -and
+                            $parameter.Name -in $setParameters.Values.Name.Where( { $_ -notin $excludedParameters } )
+                        ) {
                             # Switch variable types
                             if ( ( Get-Variable -Name $parameter.Name ).Value.GetType().Name -eq 'SwitchParameter' ) {
-                                $arraystring.Add( $arrayItem, ( Get-Variable -Name $parameter.Name ).Value.IsPresent )
+                                $bodyString.Add( $bodyKey, ( Get-Variable -Name $parameter.Name ).Value.IsPresent )
                             }
                             # All other variable types
-                            elseif ( ( Get-Variable -Name $parameter.Name ).Value -ne $null ) {
-                                $arraystring.Add( $arrayItem, ( Get-Variable -Name $parameter.Name ).Value )
+                            elseif ( ( Get-Variable -Name $parameter.Name ).Value -ne $null -and ( Get-Variable -Name $parameter.Name ).Value.Length -gt 0 ) {
+                                $bodyString.Add( $bodyKey, ( Get-Variable -Name $parameter.Name ).Value )
+                            }
+                        }
+                        elseif (
+                            ( $parameter.Name -eq $bodyKeyNoUnderscore -or $parameter.Aliases -contains $bodyKeyNoUnderscore ) -and
+                            $parameter.Name -in $setParameters.Values.Name.Where( { $_ -notin $excludedParameters } )
+                        ) {
+                            # Switch variable types
+                            if ( ( Get-Variable -Name $parameter.Name ).Value.GetType().Name -eq 'SwitchParameter' ) {
+                                $bodyString.Add( $bodyKey, ( Get-Variable -Name $parameter.Name ).Value.IsPresent )
+                            }
+                            # All other variable types
+                            elseif ( ( Get-Variable -Name $parameter.Name ).Value -ne $null -and ( Get-Variable -Name $parameter.Name ).Value.Length -gt 0 ) {
+                                $bodyString.Add( $bodyKey, ( Get-Variable -Name $parameter.Name ).Value )
                             }
                         }
                     }
                 }
+            }
 
-                $bodyString.Add( $bodyKey, @( $arraystring ) )
-            }
-            # Non-Array Object
-            else {
-                # Walk through all of the parameters defined in the function
-                # Both the parameter name and parameter alias are used to match against a body option
-                # It is suggested to make the parameter name "human friendly" and set an alias corresponding to the body option name
-                foreach ( $parameter in $Parameters.Where( { $_.Name -notin $excludedParameters } ) ) {
-                    # if the parameter name or alias matches the body option name, build a body string
-                    if (
-                        ( $parameter.Name -eq $bodyKey -or $parameter.Aliases -contains $bodyKey ) -and
-                        $parameter.Name -in $setParameters.Values.Name.Where( { $_ -notin $excludedParameters } )
-                    ) {
-                        # Switch variable types
-                        if ( ( Get-Variable -Name $parameter.Name ).Value.GetType().Name -eq 'SwitchParameter' ) {
-                            $bodyString.Add( $bodyKey, ( Get-Variable -Name $parameter.Name ).Value.IsPresent )
-                        }
-                        # All other variable types
-                        elseif ( ( Get-Variable -Name $parameter.Name ).Value -ne $null -and ( Get-Variable -Name $parameter.Name ).Value.Length -gt 0 ) {
-                            $bodyString.Add( $bodyKey, ( Get-Variable -Name $parameter.Name ).Value )
-                        }
-                    }
-                    elseif (
-                        ( $parameter.Name -eq $bodyKeyNoUnderscore -or $parameter.Aliases -contains $bodyKeyNoUnderscore ) -and
-                        $parameter.Name -in $setParameters.Values.Name.Where( { $_ -notin $excludedParameters } )
-                    ) {
-                        # Switch variable types
-                        if ( ( Get-Variable -Name $parameter.Name ).Value.GetType().Name -eq 'SwitchParameter' ) {
-                            $bodyString.Add( $bodyKey, ( Get-Variable -Name $parameter.Name ).Value.IsPresent )
-                        }
-                        # All other variable types
-                        elseif ( ( Get-Variable -Name $parameter.Name ).Value -ne $null -and ( Get-Variable -Name $parameter.Name ).Value.Length -gt 0 ) {
-                            $bodyString.Add( $bodyKey, ( Get-Variable -Name $parameter.Name ).Value )
-                        }
-                    }
-                }
-            }
+            # Store the results in a JSON string
+            $return = ConvertTo-Json -InputObject $bodyString
+
+            Write-Verbose -Message ( 'Body = {0}' -f $return )
         }
 
-        # Store the results in a JSON string
-        $bodyString = ConvertTo-Json -InputObject $bodyString
-
-        Write-Verbose -Message ( 'Body = {0}' -f $bodyString )
-
-        return $bodyString
+        $return
     } # End of process
 
     end {
