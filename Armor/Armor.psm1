21818e0c0eebc96390e1e0952557cd6ecf94c44e
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-02-21 20:54:16 -0600

    Improve variable declaration in strings

--- Armor/Armor.psm1
+++ Armor/Armor.psm1
@@ -1,7 +1,7 @@
 # Get class definition files, as well as the private and public function definition files.
-$lib = Get-ChildItem -Path "$PSScriptRoot/Lib/*.ps1" -ErrorAction 'Stop'
-$private = Get-ChildItem -Path "$PSScriptRoot/Private/*.ps1" -ErrorAction 'Stop'
-$public = Get-ChildItem -Path "$PSScriptRoot/Public/*.ps1" -ErrorAction 'Stop'
+$lib = Get-ChildItem -Path "${PSScriptRoot}/Lib/*.ps1" -ErrorAction 'Stop'
+$private = Get-ChildItem -Path "${PSScriptRoot}/Private/*.ps1" -ErrorAction 'Stop'
+$public = Get-ChildItem -Path "${PSScriptRoot}/Public/*.ps1" -ErrorAction 'Stop'
 
 # Source the definition files
 foreach ( $import in ( $lib + $private + $public ) ) {
