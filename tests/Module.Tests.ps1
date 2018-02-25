e714118d0101f41482125c50ebd9ca4e14d3b8f8
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-02-25 04:04:36 -0600

    Add more automated tests for manifest content

--- tests/Module.Tests.ps1
+++ tests/Module.Tests.ps1
@@ -1,38 +1,191 @@
-Describe 'Module' {
-    Context 'Import' {
-        It "should import module: '${env:CI_MODULE_NAME}'" {
+Describe -Name 'Module' -Tag 'Module' -Fixture {
+    Context -Name 'Import' -Fixture {
+        It -Name "should import module: '${env:CI_MODULE_NAME}'" -Test {
             { Import-Module -Name $env:CI_MODULE_MANIFEST_PATH -Force } |
                 Should -Not -Throw
         } # End of It
     } # End of Context
 
-    $moduleInfo = Import-Module -Name $env:CI_MODULE_MANIFEST_PATH -PassThru
-    $functionFileNames = ( Get-ChildItem -Path $env:CI_MODULE_PUBLIC_PATH -Filter '*.ps1' ).Name
+    <#
+    Get the last index because the script files in ScriptsToProcess are
+    returned as earlier index items.
+    #>
+    $moduleInfo = ( Import-Module -Name $env:CI_MODULE_MANIFEST_PATH -PassThru )[-1]
+
+    Context -Name 'Manifest' -Fixture {
+        $author = 'Troy Lindsay'
+        $testCases = @(
+            @{
+                'Property' = 'RootModule'
+                'Value'    = "${env:CI_MODULE_NAME}.psm1"
+            },
+            @{
+                'Property' = 'Version'
+                'Value'    = $env:CI_MODULE_VERSION
+            },
+            @{
+                'Property' = 'GUID'
+                'Value'    = '226c1ea9-1078-402a-861c-10a845a0d173'
+            },
+            @{
+                'Property' = 'Author'
+                'Value'    = $author
+            },
+            @{
+                'Property' = 'CompanyName'
+                'Value'    = 'Armor'
+            },
+            @{
+                'Property' = 'Copyright'
+                'Value'    = "(c) 2017-$( ( Get-Date ).Year ) ${author}. All rights reserved."
+            },
+            @{
+                'Property' = 'ProcessorArchitecture'
+                'Value'    = 'None'
+            },
+            @{
+                'Property' = 'Prefix'
+                'Value'    = ''
+            }
+        )
+        It -Name "should have set property: <Property> to: <Value>" -TestCases $testCases -Test {
+            param ( [String] $Property, [String] $Value )
+            $moduleInfo.$Property |
+                Should -BeExactly $Value
+        } # End of It
+
+        $testCases = @(
+            @{ 'Property' = 'CompatiblePSEditions' },
+            @{ 'Property' = 'PowerShellHostName' },
+            @{ 'Property' = 'PowerShellHostVersion' },
+            @{ 'Property' = 'DotNetFrameworkVersion' },
+            @{ 'Property' = 'ClrVersion' },
+            @{ 'Property' = 'RequiredModules' },
+            @{ 'Property' = 'RequiredAssemblies' },
+            @{ 'Property' = 'ExportedTypeFiles' },
+            @{ 'Property' = 'ExportedFormatFiles' },
+            @{ 'Property' = 'NestedModules' },
+            @{ 'Property' = 'ExportedDscResources' },
+            @{ 'Property' = 'ModuleList' },
+            @{ 'Property' = 'ReleaseNotes' },
+            @{ 'Property' = 'HelpInfoURI' }
+        )
+        It -Name "should have set property: <Property> to: `$null" -TestCases $testCases -Test {
+            param ( [String] $Property )
+            $moduleInfo.$Property |
+                Should -BeExactly $null
+        } # End of It
+
+        $testCases = $(
+            @{ 'Value' = 'Armor Complete' },
+            @{ 'Value' = 'Armor Anywhere' },
+            @{ 'Value' = 'Windows' },
+            @{ 'Value' = 'AppVeyor' },
+            @{ 'Value' = 'macOS' },
+            @{ 'Value' = 'Ubuntu Linux' },
+            @{ 'Value' = 'Travis CI' },
+            @{ 'Value' = 'Code coverage' },
+            @{ 'Value' = 'Coveralls' },
+            @{ 'Value' = 'PowerShell Gallery' }
+        )
+        It -Name "should have set property: 'Description' to contain keyword: <Value>" -TestCases $testCases -Test {
+            param ( [String] $Value )
+            $moduleInfo.Description |
+                Should -Match $Value
+        } # End of It
+
+        $testCases = @(
+            @{
+                'Property' = 'PowerShellVersion'
+                'Value'    = '5.0'
+            },
+            @{
+                'Property' = 'LicenseURI'
+                'Value'    = 'https://github.com/tlindsay42/ArmorPowerShell/blob/master/LICENSE.txt'
+            },
+            @{
+                'Property' = 'ProjectURI'
+                'Value'    = 'https://github.com/tlindsay42/ArmorPowerShell'
+            },
+            @{
+                'Property' = 'IconURI'
+                'Value'    = 'http://i.imgur.com/fbXjkCn.png'
+            }
+        )
+        It -Name "should have set property: <Property> to: <Value>" -TestCases $testCases -Test {
+            param ( [String] $Property, [String] $Value )
+            $moduleInfo.$Property.ToString() |
+                Should -BeExactly $Value
+        } # End of It
+
+        $testCases = @(
+            @{ 'Property' = 'ExportedCmdlets' }
+            @{ 'Property' = 'ExportedVariables' }
+            @{ 'Property' = 'ExportedAliases' }
+        )
+        It -Name "should have set property: <Property> to an empty collection" -TestCases $testCases -Test {
+            param ( [String] $Property )
+            $moduleInfo.$Property.Count |
+                Should -BeExactly 0
+        } # End of It
+
+        It -Name "should have set property: 'Tags'" -Test {
+            $moduleInfo.Tags.Count |
+                Should -BeGreaterThan 0
+        } # End of It
+    } # End of Context
+
+    $fileNames = Get-ChildItem -Path $env:CI_MODULE_PUBLIC_PATH -File
     $functionNames = $moduleInfo.ExportedFunctions.Keys
 
-    Context 'Import Public Advanced Function Script Files' {
-        <#
-        Checks to make sure that each script file in the module's Public directory
-        has been exported.
-        #>
-        foreach ( $functionFileName in $functionFileNames ) {
-            It "should import public advanced function script file: '${functionFileName}'" {
-                $functionFileName.Split( '.' )[0] |
+    Context -Name 'ExportedFunctions' -Fixture {
+        foreach ( $fileName in $fileNames ) {
+            It -Name "should contain entry: '${fileName}'" -Test {
+                $fileName.BaseName |
                     Should -BeIn $functionNames
+            } # End of It
+        }
+
+        foreach ( $functionName in $functionNames ) {
+            It -Name "should have matching file: '${functionName}'" -Test {
+                $functionName |
+                    Should -BeIn $fileNames.BaseName
             } # End of It
         }
     } # End of Context
 
-    Context 'Export Public Advanced Functions' {
-        <#
-        Checks to make sure that each exported function has a matching file in
-        the module's Public directory.  Check the psm1 file if any extras functions
-        are found.
-        #>
-        foreach ( $functionName in $functionNames ) {
-            It "should export public advanced function: '${functionName}'" {
-                $functionName |
-                    Should -BeIn $functionFileNames.ForEach( { $_.Split( '.' )[0] } )
+    $fileNames = Get-ChildItem -Path $env:CI_MODULE_LIB_PATH -File
+
+    Context -Name 'ScriptsToProcess' -Fixture {
+        foreach ( $fileName in $fileNames ) {
+            It -Name "should contain entry: '${fileName}'" -Test {
+                $fileName.FullName |
+                    Should -BeIn $moduleInfo.Scripts
+            } # End of It
+        }
+
+        foreach ( $fileName in $moduleInfo.Scripts ) {
+            It -Name "should have matching file: '$( Split-Path -Path $fileName -Leaf )'" -Test {
+                $fileName |
+                    Should -BeIn $fileNames.FullName
+            } # End of It
+        }
+    } # End of Context
+
+    $fileNames = Get-ChildItem -Path $env:CI_MODULE_PATH -File -Recurse
+
+    Context -Name 'FileList' -Fixture {
+        foreach ( $fileName in $fileNames ) {
+            It -Name "should contain entry: '${fileName}'" -Test {
+                $fileName.FullName |
+                    Should -BeIn $moduleInfo.FileList
+            } # End of It
+        }
+
+        foreach ( $fileName in $moduleInfo.FileList ) {
+            It -Name "should have matching file: '$( Split-Path -Path $fileName -Leaf )'" -Test {
+                $fileName |
+                    Should -BeIn $fileNames.FullName
             } # End of It
         }
     } # End of Context
