6e97963aea534f973f088ab4a609cea2efb3e1af
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-02-26 23:05:16 -0600

    Add Set-ArmorAccountContext.Tests.ps1

---
+++ tests/Set-ArmorAccountContext.Tests.ps1
@@ -0,0 +1,150 @@
+$systemUnderTest = ( Split-Path -Leaf $MyInvocation.MyCommand.Path ) -replace '\.Tests\.', '.'
+$filePath = Join-Path -Path $env:CI_MODULE_PUBLIC_PATH -ChildPath $systemUnderTest
+
+. $filePath
+
+$classFiles = Get-ChildItem -Path $env:CI_MODULE_LIB_PATH
+foreach ( $classFile in $classFiles ) {
+    . $classFile.FullName
+}
+
+$privateFunctionFiles = Get-ChildItem -Path $env:CI_MODULE_PRIVATE_PATH
+foreach ( $privateFunctionFile in $privateFunctionFiles ) {
+    . $privateFunctionFile.FullName
+}
+
+$Global:ArmorSession = [ArmorSession]::New()
+$Global:ArmorSession.Accounts += [ArmorAccount] @{
+    'ID'       = 1
+    'Name'     = 'Armor Defense, Inc.'
+    'Currency' = 'USD'
+    'Status'   = 'Claimed'
+    'Parent'   = -1
+    'Products' = @()
+}
+$Global:ArmorSession.Accounts += [ArmorAccount] @{
+    'ID'       = 2
+    'Name'     = 'Old Armor QA Account, Ltd'
+    'Currency' = 'GBP'
+    'Status'   = 'Cancelled'
+    'Parent'   = 1
+    'Products' = @()
+}
+
+$function = $systemUnderTest.Split( '.' )[0]
+$describe = $Global:PublicFunctionForm -f $function
+Describe -Name $describe -Tag 'Function', 'Public', $function -Fixture {
+    #region init
+    $help = Get-Help -Name $function -Full
+    #endregion
+
+    Context -Name $Global:FunctionName -Fixture {
+        $testName = $Global:ShouldBeForm -f $function
+        It -Name $testName -Test {
+            $help.Name |
+                Should -BeExactly $function
+        }
+    } # End of Context
+
+    Context -Name $Global:FunctionHelpContext -Fixture {
+        $testCases = @(
+            @{ 'Property' = 'Synopsis' },
+            @{ 'Property' = 'Description' }
+        )
+        It -Name $Global:FunctionHelpContentForm -TestCases $testCases -Test {
+            param ( [String] $Property )
+            $help.$Property.Length |
+                Should -BeGreaterThan 0
+        } # End of It
+
+        $value = 'System.UInt16'
+        $testName = $Global:FunctionHelpSpecificContentForm -f 'Inputs', $value
+        It -Name $testName -Test {
+            $help.InputTypes.InputType.Type.Name |
+                Should -BeExactly $value
+        } # End of It
+
+        $value = 'ArmorAccount'
+        $testName = $Global:FunctionHelpSpecificContentForm -f 'Outputs', $value
+        It -Name $testName -Test {
+            $help.ReturnValues.ReturnValue.Type.Name |
+                Should -BeExactly $value
+        } # End of It
+
+        $value = $Global:FunctionHelpNotes
+        $testName = $Global:FunctionHelpSpecificContentForm -f 'Notes', ( $value -replace '\n', ', ' )
+        It -Name $testName -Test {
+            $help.AlertSet.Alert.Text |
+                Should -BeExactly $value
+        } # End of It
+
+        $testName = $Global:FunctionHelpExampleEntry
+        It -Name $testName -Test {
+            $help.Examples.Example.Remarks.Length |
+                Should -BeGreaterThan 0
+        } # End of It
+
+        $testName = $Global:FunctionHelpLinkEntry
+        It -Name $testName -Test {
+            $help.RelatedLinks.NavigationLink.Uri.Count |
+                Should -BeGreaterThan 3
+        } # End of It
+
+        foreach ( $uri in $help.RelatedLinks.NavigationLink.Uri ) {
+            $testName = $Global:FunctionHelpLinkValidForm -f $uri
+            It -Name $testName -Test {
+                ( Invoke-WebRequest -Method 'Get' -Uri $uri ).StatusCode |
+                    Should -Be 200
+            } # End of It
+        }
+    } # End of Context
+
+    Context -Name 'Parameters' -Fixture {
+        $value = 1
+        $testName = $Global:FunctionParameterCountForm -f $value
+        It -Name $testName -TestCases $testCases -Test {
+            @( $help.Parameters.Parameter ).Count |
+                Should -Be $value
+        } # End of It
+
+        $testCases = @(
+            @{ 'Name' = 'ID' }
+        )
+        $testName = $Global:FunctionParameterNameForm
+        It -Name $testName -TestCases $testCases -Test {
+            param ( [String] $Name )
+            $Name |
+                Should -BeIn $help.Parameters.Parameter.Name
+        } # End of It
+    } # End of Context
+
+    Context -Name 'Execution' -Fixture {
+        Mock -CommandName Get-ArmorIdentity -Verifiable -MockWith {
+            @{ 'Accounts' = @{ 'ID' = 1, 2 } }
+        }
+        Mock -CommandName Test-ArmorSession -Verifiable -MockWith {}
+
+        $testName = 'should fail to set the account context'
+        It -Name $testName -Test {
+            { Set-ArmorAccountContext -ID 3 } |
+                Should -Throw
+        } # End of It
+
+        $value = 2
+        $testName = "should set the account context"
+        It -Name $testName -Test {
+            ( Set-ArmorAccountContext -ID $value ).ID |
+                Should -Be $value
+        } # End of It
+
+        $testName = $Global:MethodTypeForm
+        It -Name $testName -Test {
+            Set-ArmorAccountContext -ID 1 |
+                Should -BeOfType ( [ArmorAccount] )
+        } # End of It
+
+        Assert-VerifiableMock
+        Assert-MockCalled -CommandName Get-ArmorIdentity -Times 3
+        Assert-MockCalled -CommandName Test-ArmorSession -Times 2
+    } # End of Context
+} # End of Describe
