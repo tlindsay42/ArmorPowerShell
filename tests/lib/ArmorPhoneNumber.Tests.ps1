01c01493ad221a8e2adb3b83cd6e72c8e2fe78db
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-05-29 19:20:46 -0500

    Add ArmorPhoneNumber.Tests.ps1

---
+++ tests/lib/ArmorPhoneNumber.Tests.ps1
@@ -0,0 +1,125 @@
+$systemUnderTest = ( Split-Path -Leaf $MyInvocation.MyCommand.Path ) -replace '\.Tests\.', '.'
+$filePath = Join-Path -Path $env:CI_MODULE_LIB_PATH -ChildPath $systemUnderTest
+
+. $filePath
+
+$class = $systemUnderTest.Split( '.' )[0]
+$describe = $Global:ClassForm -f $class
+Describe -Name $describe -Tag 'Class', $class -Fixture {
+    #region init
+    #endregion
+
+    Context -Name $Global:Constructors -Fixture {
+        It -Name $Global:DefaultConstructorForm -Test {
+            { [ArmorPhoneNumber]::New() } |
+                Should -Not -Throw
+        } # End of It
+    } # End of Context
+
+    [ArmorPhoneNumber] $temp = [ArmorPhoneNumber]::New()
+
+    $property = 'CountryCode'
+    $context = $Global:PropertyForm -f $property
+    Context -Name $context -Fixture {
+        $testCases = @(
+            @{ 'Value' = 0 },
+            @{ 'Value' = 1000 }
+        )
+        It -Name $Global:PropertyFailForm -TestCases $testCases -Test {
+            param ( [UInt16] $Value )
+            { $temp.$property = $Value } |
+                Should -Throw
+        } # End of It
+
+        $testCases = @(
+            @{ 'Value' = 1 },
+            @{ 'Value' = 999 }
+        )
+        It -Name $Global:PropertyPassForm -TestCases $testCases -Test {
+            param ( [UInt16] $Value )
+            { $temp.$property = $Value } |
+                Should -Not -Throw
+        } # End of It
+
+        It -Name $Global:PropertyTypeForm -Test {
+            $temp.$property |
+                Should -BeOfType ( [System.UInt16] )
+        } # End of It
+    } # End of Context
+
+    $property = 'CountryIsoCode'
+    $context = $Global:PropertyForm -f $property
+    Context -Name $context -Fixture {
+        $testCases = @(
+            @{ 'Value' = '' },
+            @{ 'Value' = '1' },
+            @{ 'Value' = '999' },
+            @{ 'Value' = '1000' }
+        )
+        It -Name $Global:PropertyPassForm -TestCases $testCases -Test {
+            param ( [String] $Value )
+            { $temp.$property = $Value } |
+                Should -Not -Throw
+        } # End of It
+
+        It -Name $Global:PropertyTypeForm -Test {
+            $temp.$property |
+                Should -BeOfType ( [System.String] )
+        } # End of It
+    } # End of Context
+
+    $property = 'Number'
+    $context = $Global:PropertyForm -f $property
+    Context -Name $context -Fixture {
+        $testCases = @(
+            @{ 'Value' = '' }
+        )
+        It -Name $Global:PropertyFailForm -TestCases $testCases -Test {
+            param ( [String] $Value )
+            { $temp.$property = $Value } |
+                Should -Throw
+        } # End of It
+
+        $testCases = @(
+            @{ 'Value' = '5558675309' },
+            @{ 'Value' = '555.867.5309' },
+            @{ 'Value' = '555 867 5309' },
+            @{ 'Value' = '555-867-5309' },
+            @{ 'Value' = '(555) 867-5309' },
+            @{ 'Value' = '(5555) 867 5309' },
+            @{ 'Value' = '(555) 5867 5309' }
+        )
+        It -Name $Global:PropertyPassForm -TestCases $testCases -Test {
+            param ( [String] $Value )
+            { $temp.$property = $Value } |
+                Should -Not -Throw
+        } # End of It
+
+        It -Name $Global:PropertyTypeForm -Test {
+            $temp.$property |
+                Should -BeOfType ( [System.String] )
+        } # End of It
+    } # End of Context
+
+    $property = 'PhoneExt'
+    $context = $Global:PropertyForm -f $property
+    Context -Name $context -Fixture {
+        $testCases = @(
+            @{ 'Value' = '' },
+            @{ 'Value' = '1' },
+            @{ 'Value' = ',,,5555' }
+        )
+        It -Name $Global:PropertyPassForm -TestCases $testCases -Test {
+            param ( [String] $Value )
+            { $temp.$property = $Value } |
+                Should -Not -Throw
+        } # End of It
+
+        It -Name $Global:PropertyTypeForm -Test {
+            $temp.$property |
+                Should -BeOfType ( [System.String] )
+        } # End of It
+    } # End of Context
+
+    Remove-Variable -Name 'temp'
+} # End of Describe
