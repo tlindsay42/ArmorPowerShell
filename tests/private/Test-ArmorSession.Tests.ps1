222558d9018f53942a9ee4e6dc86590d1ec7e072
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-05-31 01:01:48 -0500

    Update Test-ArmorSession negative test $Session data type

--- tests/private/Test-ArmorSession.Tests.ps1
+++ tests/private/Test-ArmorSession.Tests.ps1
@@ -70,7 +70,7 @@ Describe -Name $describe -Tag 'Function', 'Private', $function -Fixture {
         )
         $testName = 'should fail when the session authorization is: <Authorization>'
         It -Name $testName -TestCases $testCases -Test {
-            param ( [ArmorSession] $Session, [String] $Authorization )
+            param ( [PSCustomObject] $Session, [String] $Authorization )
             {
                 [ArmorSession] $Global:ArmorSession = $Session
                 $Global:ArmorSession.Headers.Authorization = $Authorization
