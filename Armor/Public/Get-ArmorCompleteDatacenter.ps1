7dd28d70cf0fc4b02ef17285744421ba1d5cae46
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-06-08 09:58:37 -0500

    Update Get-ArmorCompleteDatacenter help documentation
        * #2
        * #68: PlatyPS compatible

--- Armor/Public/Get-ArmorCompleteDatacenter.ps1
+++ Armor/Public/Get-ArmorCompleteDatacenter.ps1
@@ -1,12 +1,12 @@
 function Get-ArmorCompleteDatacenter {
     <#
         .SYNOPSIS
-        This cmdlet retrieves Armor Complete datacenters.
+        Retrieves Armor Complete datacenter details.
 
         .DESCRIPTION
-        This cmdlet retrieves details about the Armor Complete datacenters,
-        regions, and compute zones.  Returns a set of datacenters that
-        correspond to the filter criteria provided by the cmdlet parameters.
+        Retrieves details about the Armor Complete datacenters, regions, and compute
+        zones.  Returns a set of datacenters that correspond to the filter criteria
+        provided by the cmdlet parameters.
 
 
         .INPUTS
@@ -17,58 +17,30 @@ function Get-ArmorCompleteDatacenter {
         PSCustomObject
 
         .NOTES
-        Troy Lindsay
-        Twitter: @troylindsay42
-        GitHub: tlindsay42
+        - Troy Lindsay
+        - Twitter: @troylindsay42
+        - GitHub: tlindsay42
 
         .EXAMPLE
-        Get-ArmorCompleteDatacenter |
-            Select-Object -Property ID, Location, Name
-
-        ID Location Name
-        -- -------- ----
-         1 DFW01    US Central
-         2 PHX01    US West
-         3 LHR01    EU West
-         4 AMS01    EU Central
-         5 SIN01    AS East
-
-
-        Description
-        -----------
-        Gets the Armor Complete datacenters and filters the compute zones.
+        Get-ArmorCompleteDatacenter
+        Retrieves the details for all Armor Complete datacenters.
 
         .EXAMPLE
-        Get-ArmorCompleteDatacenter -ID 2 |
-            Select-Object -Property ID, Location, Name
-
-        ID Location Name
-        -- -------- ----
-         2 PHX01    US West
-
-
-        Description
-        -----------
-        Gets the Armor Complete datacenter with ID=2 and filters the compute
-        zones.
+        Get-ArmorCompleteDatacenter -ID 2
+        Retrieves the details for the Armor Complete datacenter with ID=2.
 
         .EXAMPLE
-        1, 'PHX01' | Get-ArmorCompleteDatacenter |
-            Select-Object -Property ID, Location, Name
+        1, 'PHX01' | Get-ArmorCompleteDatacenter
+        Retrieves the details for the Armor Complete datacenter with ID=1 and
+        Location='PHX01' via pipeline values.
 
-        ID Location Name
-        -- -------- ----
-         1 DFW01    US Central
-         2 PHX01    US West
-
-
-        Description
-        -----------
-        Gets the Armor Complete datacenter with ID=1 and Location='PHX01' and
-        filters the compute zones.
+        .EXAMPLE
+        [PSCustomObject] @{ 'Location' = 'EU West' } | Get-ArmorCompleteDatacenter
+        Retrieves the details for the Armor Complete datacenter with Name='EU West' via
+        property name in the pipeline.
 
         .LINK
-        http://armorpowershell.readthedocs.io/en/latest/cmd_get.html#get-armorcompletedatacenter
+        https://armorpowershell.readthedocs.io/en/latest/cmd_get.html#get-armorcompletedatacenter
 
         .LINK
         https://github.com/tlindsay42/ArmorPowerShell/blob/master/Armor/Public/Get-ArmorCompleteDatacenter.ps1
@@ -84,9 +56,7 @@ function Get-ArmorCompleteDatacenter {
     [OutputType( [ArmorCompleteDatacenter[]] )]
     [OutputType( [ArmorCompleteDatacenter] )]
     param (
-        <#
-        Specifies the ID of the Armor Complete datacenter.
-        #>
+        # Specifies the ID of the Armor Complete datacenter.
         [Parameter(
             ParameterSetName = 'ID',
             Position = 0,
@@ -97,9 +67,7 @@ function Get-ArmorCompleteDatacenter {
         [UInt16]
         $ID = 0,
 
-        <#
-        Specifies the name of the Armor Complete region.
-        #>
+        # Specifies the name of the Armor Complete region.
         [Parameter(
             ParameterSetName = 'Name',
             ValueFromPipelineByPropertyName = $true
@@ -108,9 +76,7 @@ function Get-ArmorCompleteDatacenter {
         [String]
         $Name = '',
 
-        <#
-        Specifies the name of the Armor Complete datacenter.
-        #>
+        # Specifies the name of the Armor Complete datacenter.
         [Parameter(
             ParameterSetName = 'Location',
             Position = 0,
@@ -121,9 +87,7 @@ function Get-ArmorCompleteDatacenter {
         [String]
         $Location = '',
 
-        <#
-        Specifies the API version for this request.
-        #>
+        # Specifies the API version for this request.
         [Parameter( Position = 1 )]
         [ValidateSet( 'v1.0' )]
         [String]
