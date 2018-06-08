143d5150d3fb5a5e0005817574693af7f77d6e56
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-06-08 09:43:23 -0500

    Update Invoke-ArmorWebRequest help documentation
        * #2
        * #68: PlatyPS compatible

--- Armor/Public/Invoke-ArmorWebRequest.ps1
+++ Armor/Public/Invoke-ArmorWebRequest.ps1
@@ -4,9 +4,8 @@ function Invoke-ArmorWebRequest {
         This cmdlet submits web requests to the Armor API.
 
         .DESCRIPTION
-        This cmdlet sends custom HTTPS requests to the Armor API.  It can
-        be used for calling API endpoints that are not yet covered by the
-        cmdlets in this module.
+        This cmdlet sends custom HTTPS requests to the Armor API.  It can be used for
+        calling API endpoints that are not yet covered by the cmdlets in this module.
 
         .INPUTS
         String
@@ -14,15 +13,20 @@ function Invoke-ArmorWebRequest {
         PSCustomObject
 
         .NOTES
-        Troy Lindsay
-        Twitter: @troylindsay42
-        GitHub: tlindsay42
+        - Troy Lindsay
+        - Twitter: @troylindsay42
+        - GitHub: tlindsay42
 
         .EXAMPLE
-        {required: show one or more examples using the function}
+        Invoke-ArmorWebRequest -Endpoint '/me'
+        Retrieves the current user's identity details.
+
+        .EXAMPLE
+        Invoke-ArmorWebRequest -Endpoint '/vms' -Headers $Global:ArmorSession.Headers
+        Retrieves VM details using the session headers.
 
         .LINK
-        http://armorpowershell.readthedocs.io/en/latest/cmd_invoke.html#invoke-armorwebrequest
+        https://armorpowershell.readthedocs.io/en/latest/cmd_invoke.html#invoke-armorwebrequest
 
         .LINK
         https://github.com/tlindsay42/ArmorPowerShell/blob/master/Armor/Public/Invoke-ArmorWebRequest.ps1
@@ -38,9 +42,7 @@ function Invoke-ArmorWebRequest {
     [OutputType( [PSCustomObject[]] )]
     [OutputType( [PSCustomObject] )]
     param (
-        <#
-        Specifies the Armor API endpoint.
-        #>
+        # Specifies the Armor API endpoint.
         [Parameter(
             Mandatory = $true,
             HelpMessage = 'Please enter the Armor API endpoint',
@@ -50,17 +52,15 @@ function Invoke-ArmorWebRequest {
         [String]
         $Endpoint,
 
-        <#
-        Specifies the headers of the Armor API web request.
-        #>
+        # Specifies the headers of the Armor API web request.
         [Parameter( Position = 1 )]
         [ValidateNotNull()]
         [Hashtable]
         $Headers = $Global:ArmorSession.Headers,
 
         <#
-        Specifies the method used for the Armor API web request.  The permitted
-        values are:
+        Specifies the method used for the Armor API web request.  The permitted values
+        are:
         - Delete
         - Get
         - Patch
@@ -73,8 +73,8 @@ function Invoke-ArmorWebRequest {
         $Method = 'Get',
 
         <#
-        Specifies the body of the Armor API web request.  This parameter is
-        ignored for Get requests.
+        Specifies the body of the Armor API web request.  This parameter is ignored for
+        Get requests.
         #>
         [Parameter(
             Position = 3,
@@ -86,8 +86,8 @@ function Invoke-ArmorWebRequest {
         $Body = '',
 
         <#
-        Specifies the value of the HTTP response code that indicates success
-        for this Armor API web request.
+        Specifies the value of the HTTP response code that indicates success for this
+        Armor API web request.
         #>
         [Parameter( Position = 4 )]
         [ValidateSet( 200, 202 )]
@@ -95,9 +95,9 @@ function Invoke-ArmorWebRequest {
         $SuccessCode = 200,
 
         <#
-        If the PowerShell $ConfirmPreference value is elevated for this Armor
-        API web request by setting the -Confirm parameter to $true, this
-        specifies the text to display at the user prompt.
+        If the PowerShell $ConfirmPreference value is elevated for this Armor API web
+        request by setting the -Confirm parameter to $true, this specifies the text to
+        display at the user prompt.
         #>
         [Parameter( Position = 5 )]
         [ValidateNotNullorEmpty()]
