f5647880659670ea04bf97df93f5ac78e080093a
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-06-08 10:04:34 -0500

    Update Disconnect-Armor help documentation
        * #2
        * #68: PlatyPS compatible

--- Armor/Public/Disconnect-Armor.ps1
+++ Armor/Public/Disconnect-Armor.ps1
@@ -4,33 +4,24 @@ function Disconnect-Armor {
         Disconnects from Armor and destroys the session information.
 
         .DESCRIPTION
-        Disconnects from the Armor API and destroys the $Global:ArmorSession
-        session variable.
+        Disconnects from the Armor API and destroys the $Global:ArmorSession session
+        variable.
 
         .INPUTS
-        None- you cannot pipe objects to this cmdlet.
+        None- this function does not accept pipeline inputs.
 
         .NOTES
-        Troy Lindsay
-        Twitter: @troylindsay42
-        GitHub: tlindsay42
+        - Troy Lindsay
+        - Twitter: @troylindsay42
+        - GitHub: tlindsay42
 
         .EXAMPLE
         Disconnect-Armor
-        Performing the operation "Disconnect" on target "Armor session".
-        [Y] Yes  [A] Yes to All  [N] No  [L] No to All  [S] Suspend  [?] Help (default is "Y"): Y
-
-        C:\>( Get-Variable -Scope Global ).Where( { $_.Name -eq 'ArmorSession' } ).Count
-        0
-
-
-        Description
-        -----------
-        Disconnects from the Armor API, destroys the $Global:ArmorSession, and
-        then proves that the global scope ArmorSession variable no longer exists.
+        Disconnects from the Armor API and destroys the $Global:ArmorSession session
+        variable.
 
         .LINK
-        http://armorpowershell.readthedocs.io/en/latest/cmd_disconnect.html#disconnect-armor
+        https://armorpowershell.readthedocs.io/en/latest/cmd_disconnect.html#disconnect-armor
 
         .LINK
         https://github.com/tlindsay42/ArmorPowerShell/blob/master/Armor/Public/Disconnect-Armor.ps1
