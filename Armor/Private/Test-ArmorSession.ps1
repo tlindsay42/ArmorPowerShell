cf2086af7d8598aad111a511572ed1eb990ec3c9
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-06-08 10:18:38 -0500

    Update Test-ArmorSession help documentation
            * #2
            * #68: PlatyPS compatible

--- Armor/Private/Test-ArmorSession.ps1
+++ Armor/Private/Test-ArmorSession.ps1
@@ -1,36 +1,24 @@
 function Test-ArmorSession {
     <#
         .SYNOPSIS
-        This cmdlet tests the Armor API session.
+        Tests the validity of the Armor API session.
 
         .DESCRIPTION
-        Test to see if a session has been established with the Armor API and
-        that it has not yet expired.  If no token is found, an error will be
-        thrown.  If the session has expired, Disconnect-Armor will be called
-        with confirmation disabled to clean up the session.  If less than 2/3
-        of the session length remain, Update-ArmorApiToken will be called to
-        renew the session.
+        Test to see if a session has been established with the Armor API and that it has not yet expired.  If no token is found, an error will be thrown.  If the session has expired, Disconnect-Armor will be called with confirmation disabled to clean up the session.  If less than 2/3 of the session length remain, Update-ArmorApiToken will be called to renew the session.
 
-        This cmdlet should be called in the Begin section of public cmdlets
-        for optimal performance, so that the session is not tested repeatedly
-        when pipeline input is processed.
+        This cmdlet should be called in the Begin section of public cmdlets for optimal performance, so that the session is not tested repeatedly when pipeline input is processed.
 
         .INPUTS
-        None- you cannot pipe objects to this cmdlet.
+        None- this function does not accept pipeline inputs.
 
         .NOTES
-        Troy Lindsay
-        Twitter: @troylindsay42
-        GitHub: tlindsay42
+        - Troy Lindsay
+        - Twitter: @troylindsay42
+        - GitHub: tlindsay42
 
         .EXAMPLE
         Test-ArmorSession
-
-
-        Description
-        -----------
-        Validates that the Armor API session stored in $Global:ArmorSession is
-        still active.
+        Validates that the Armor API session stored in $Global:ArmorSession is still active.
 
         .LINK
         https://armorpowershell.readthedocs.io/en/latest/index.html
