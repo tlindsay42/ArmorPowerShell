053dc2ba6f57516233d9ec6738177e061d66704d
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-06-10 05:06:00 -0500

    Replace ReadTheDocs & ReStructureText build tasks & data w/ GitHub Pages
    * #68
    * #71
    * #83

--- build/Build-Project.ps1
+++ build/Build-Project.ps1
@@ -18,14 +18,16 @@ $text = @{
     'CoverallsProjectUrl'   = "https://coveralls.io/github/${Env:CI_OWNER_NAME}/${Env:CI_PROJECT_NAME}?branch=master"
     'CurrentVersion'        = 'Current Version'
     'DocumentationStatus'   = 'Documentation Status'
+    'GitHubPages'           = 'GitHubPages'
+    'GitHubPagesProjectUrl' = "https://${Env:CI_OWNER_NAME}.github.io/${Env:CI_PROJECT_NAME}/"
     'Gitter'                = 'Gitter'
-    'GitterImageAlt'        = 'Join the chat at https://gitter.im/ArmorPowerShell/Lobby'
-    'GitterImageUrl'        = 'https://badges.gitter.im/ArmorPowerShell/Lobby.svg'
-    'GitterProjectUrl'      = 'https://gitter.im/ArmorPowerShell/Lobby?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge'
+    'GitterImageAlt'        = "Join the chat at https://gitter.im/${Env:CI_PROJECT_NAME}/Lobby"
+    'GitterImageUrl'        = "https://badges.gitter.im/${Env:CI_PROJECT_NAME}/Lobby.svg"
+    'GitterProjectUrl'      = "https://gitter.im/${Env:CI_PROJECT_NAME}/Lobby?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge"
     'LatestBuild'           = "${Env:CI_PROJECT_NAME}: Latest Build"
     'macOS'                 = 'macOS'
     'MdBoldLinkForm'        = "**[{0}]({1} '{2}')**"
-    'MdImageForm'           = '[![{0}]({1})]({2}) '
+    'MdImageForm'           = '[![{0}]({1})]({2})'
     'MdLinkForm'            = "[{0}]({1} '{2}')"
     'Pester'                = 'Pester'
     'PesterUrl'             = 'https://github.com/pester/Pester'
@@ -34,16 +36,9 @@ $text = @{
     'PSGallery'             = 'PowerShell Gallery'
     'PSGalleryImageUrl'     = "https://img.shields.io/powershellgallery/v/${Env:CI_MODULE_NAME}.svg"
     'PSGalleryProjectUrl'   = "https://www.powershellgallery.com/packages/${Env:CI_MODULE_NAME}"
-    'ReadTheDocs'           = 'ReadTheDocs'
-    'ReadTheDocsImageUrl'   = "https://readthedocs.org/projects/${projectNameLowerCase}/badge/?version=latest"
-    'ReadTheDocsProjectUrl' = "http://${projectNameLowerCase}.readthedocs.io/en/latest/?badge=latest"
     'RepoUrl'               = "https://github.com/${Env:CI_OWNER_NAME}/${Env:CI_PROJECT_NAME}"
     'RestfulApi'            = 'RESTful APIs'
-    'RstExplicitLineBreak'  = "|`n`n"
-    'RstLinkForm'           = '`{0}`_'
-    'RstImageForm'          = ".. image:: {0}`n   :target: {1}`n   :alt: {2}`n`n"
-    'RstLinkMap'            = ".. _{0}: {1}`n`n"
-    'Title'                 = 'Armor PowerShell Module'
+    'Title'                 = 'The Armor Community PowerShell Module'
     'TotalDownloads'        = 'Total Downloads'
     'TravisCi'              = 'Travis CI'
     'TravisCiImageUrl'      = "https://travis-ci.org/${Env:CI_OWNER_NAME}/${Env:CI_PROJECT_NAME}.svg?branch=master"
@@ -63,9 +58,9 @@ $text += @{
     'ArmorAnywhereMdLinkTitle' = "$( $text.ArmorAnywhere ) $( $text.ProductPage )"
     'ArmorCompleteMdLinkTitle' = "$( $text.ArmorComplete ) $( $text.ProductPage )"
     'CoverallsMdLinkTitle'     = "$( $text.Coveralls ): ${Env:CI_PROJECT_NAME}: Latest Report"
+    'GitHubPagesMdLinkTitle'   = "$( $text.GitHubPages ): $( $text.LatestBuild )"
     'PesterMdLinkTitle'        = "$( $text.Pester ) GitHub repo"
     'PSGalleryMdLinkTitle'     = $text.PSGallery
-    'ReadTheDocsMdLinkTitle'   = "$( $text.ReadTheDocs ): $( $text.LatestBuild )"
     'RestfulApiMdLinkTitle'    = 'Armor API Guide'
     'TravisCiMdLinkTitle'      = "$( $text.TravisCi ): $( $text.LatestBuildConsole )"
 }
@@ -74,47 +69,20 @@ $text += @{
 $text += @{
     'AppVeyorMd'           = $text.MdLinkForm -f $text.AppVeyor, $text.AppVeyorProjectUrl, $text.AppVeyorMdLinkTitle
     'AppVeyorMdShield'     = $text.MdImageForm -f $text.BuildStatus, $text.AppVeyorImageUrl, $text.AppVeyorProjectUrl
-    'AppVeyorRst'          = $text.RstLinkForm -f $text.AppVeyor
-    'AppVeyorRstMap'       = $text.RstLinkMap -f $text.AppVeyor, $text.AppVeyorProjectUrl
-    'AppVeyorRstShield'    = $text.RstImageForm -f $text.AppVeyorImageUrl, $text.AppVeyorProjectUrl, $text.BuildStatus
     'ArmorAnywhereMd'      = $text.MdBoldLinkForm -f $text.ArmorAnywhere, $text.ArmorAnywhereUrl, $text.ArmorAnywhereMdLinkTitle
-    'ArmorAnywhereRst'     = $text.RstLinkForm -f $text.ArmorAnywhere
-    'ArmorAnywhereRstMap'  = $text.RstLinkMap -f $text.ArmorAnywhere, $text.ArmorAnywhereUrl
     'ArmorCompleteMd'      = $text.MdBoldLinkForm -f $text.ArmorComplete, $text.ArmorCompleteUrl, $text.ArmorCompleteMdLinkTitle
-    'ArmorCompleteRst'     = $text.RstLinkForm -f $text.ArmorComplete
-    'ArmorCompleteRstMap'  = $text.RstLinkMap -f $text.ArmorComplete, $text.ArmorCompleteUrl
     'CoverallsMd'          = $text.MdLinkForm -f $text.Coveralls, $text.CoverallsProjectUrl, $text.CoverallsMdLinkTitle
     'CoverallsMdShield'    = $text.MdImageForm -f $text.CoverageStatus, $text.CoverallsImageUrl, $text.CoverallsProjectUrl
-    'CoverallsRst'         = $text.RstLinkForm -f $text.Coveralls
-    'CoverallsRstMap'      = $text.RstLinkMap -f $text.Coveralls, $text.CoverallsProjectUrl
-    'CoverallsRstShield'   = $text.RstImageForm -f $text.CoverallsImageUrl, $text.CoverallsProjectUrl, $text.CoverageStatus
-    'GitHubRst'            = $text.RstLinkForm -f $text.AvailableOnGitHub
-    'GitHubRstMap'         = $text.RstLinkMap -f $text.AvailableOnGitHub, $repoUrl
+    'GitHubPagesMd'        = $text.MdBoldLinkForm -f 'full documentation', $text.GitHubPagesProjectUrl, $text.GitHubPagesMdLinkTitle
     'GitterMdShield'       = $text.MdImageForm -f $text.GitterImageAlt, $text.GitterImageUrl, $text.GitterProjectUrl
-    'GitterRstMap'         = $text.RstLinkMap -f $text.Gitter, $text.GitterProjectUrl
-    'GitterRstShield'      = $text.RstImageForm -f $text.GitterImageUrl, $text.GitterProjectUrl, $text.GitterImageAlt
     'macOSBold'            = $text.BoldForm -f $text.macOS
     'PesterMd'             = $text.MdLinkForm -f $text.Pester, $text.PesterUrl, $text.PesterMdLinkTitle
-    'PesterRst'            = $text.RstLinkForm -f $text.Pester
-    'PesterRstMap'         = $text.RstLinkMap -f $text.Pester, $text.PesterUrl
     'PSDownloadsMdShield'  = $text.MdImageForm -f $text.TotalDownloads, $text.PSDownloadsImageUrl, $text.PSGalleryProjectUrl
-    'PSDownloadsRstShield' = $text.RstImageForm -f $text.PSDownloadsImageUrl, $text.PSGalleryProjectUrl, $text.TotalDownloads
     'PSGalleryMd'          = $text.MdLinkForm -f $text.PSGallery, $text.PSGalleryProjectUrl, $text.PSGalleryMdLinkTitle
     'PSGalleryMdShield'    = $text.MdImageForm -f $text.CurrentVersion, $text.PSGalleryImageUrl, $text.PSGalleryProjectUrl
-    'PSGalleryRst'         = $text.RstLinkForm -f $text.PSGallery
-    'PSGalleryRstMap'      = $text.RstLinkMap -f $text.PSGallery, $text.PSGalleryProjectUrl
-    'PSGalleryRstShield'   = $text.RstImageForm -f $text.PSGalleryImageUrl, $text.PSGalleryProjectUrl, $text.CurrentVersion
-    'ReadTheDocsMd'        = $text.MdBoldLinkForm -f 'full documentation', $text.ReadTheDocsProjectUrl, $text.ReadTheDocsMdLinkTitle
-    'ReadTheDocsMdShield'  = $text.MdImageForm -f $text.DocumentationStatus, $text.ReadTheDocsImageUrl, $text.ReadTheDocsProjectUrl
-    'ReadTheDocsRstShield' = $text.RstImageForm -f $text.ReadTheDocsImageUrl, $text.ReadTheDocsProjectUrl, $text.DocumentationStatus
     'RestfulApiMd'         = $text.MdLinkForm -f $text.RestfulApi, $text.ArmorApiGuideUrl, $text.RestfulApiMdLinkTitle
-    'RestfulApiRst'        = $text.RstLinkForm -f $text.RestfulApi
-    'RestfulApiRstMap'     = $text.RstLinkMap -f $text.RestfulApi, $text.ArmorApiGuideUrl
     'TravisCiMd'           = $text.MdLinkForm -f $text.TravisCi, $text.TravisCiProjectUrl, $text.TravisCiMdLinkTitle
     'TravisCiMdShield'     = $text.MdImageForm -f $text.BuildStatus, $text.TravisCiImageUrl, $text.TravisCiProjectUrl
-    'TravisCiRst'          = $text.RstLinkForm -f $text.TravisCi
-    'TravisCiRstMap'       = $text.RstLinkMap -f $text.TravisCi, $text.TravisCiProjectUrl
-    'TravisCiRstShield'    = $text.RstImageForm -f $text.TravisCiImageUrl, $text.TravisCiProjectUrl, $text.BuildStatus
     'UbuntuBold'           = $text.BoldForm -f $text.Ubuntu
     'WindowsBold'          = $text.BoldForm -f $text.Windows
 }
@@ -221,119 +189,44 @@ $markDownDescription = $description -replace
     $text.Coveralls, $text.CoverallsMd -replace
     $text.PSGallery, $text.PSGalleryMd
 
+$readmePath = Join-Path -Path $Env:CI_BUILD_PATH -ChildPath 'README.md' -ErrorAction 'Stop'
 # Build README.md
-$content = (
-    "# $( $text.Title )`r`n`r`n" +
+"# $( $text.Title )`r`n`r`n" +
     $text.PSGalleryMdShield + "`r`n" +
     $text.PSDownloadsMdShield + "`r`n`r`n" +
     $text.AppVeyorMdShield + "`r`n" +
     $text.TravisCiMdShield + "`r`n" +
     $text.CoverallsMdShield + "`r`n" +
-    $text.ReadTheDocsMdShield + "`r`n`r`n" +
     $text.GitterMdShield + "`r`n`r`n" +
     "${markDownDescription}`r`n`r`n" +
-    "Please visit the $( $text.ReadTheDocsMd ) for more details."
-) |
-    Out-File -FilePath "${Env:CI_BUILD_PATH}/README.md" -Encoding 'utf8'
+    "Please visit the $( $text.GitHubPagesMd ) for more details." |
+    Out-File -FilePath $readmePath -Encoding 'utf8' -Force -ErrorAction 'Stop'
 
-# Enrich keywords in the description for use in index.rst (reStructuredText formatting)
-$reStructuredTextDescription = $description -replace
-    $text.ArmorComplete, $text.ArmorCompleteRst -replace
-    $text.ArmorAnywhere, $text.ArmorAnywhereRst -replace
-    $text.RestfulApi, $text.RestfulApiRst -replace
-    $text.Windows, $text.WindowsBold -replace
-    $text.AppVeyor, $text.AppVeyorRst -replace
-    $text.macOS, $text.macOSBold -replace
-    $text.Ubuntu, $text.UbuntuBold -replace
-    $text.TravisCi, $text.TravisCiRst -replace
-    $text.Pester, $text.PesterRst -replace
-    $text.Coveralls, $text.CoverallsRst -replace
-    $text.PSGallery, $text.PSGalleryRst -replace
-    "`r", ''
+$indexPath = Join-Path -Path $Env:CI_DOCS_PATH -ChildPath 'index.md' -ErrorAction 'Stop'
+Copy-Item -Path $readmePath -Destination $indexPath -Force -ErrorAction 'Stop'
 
-$content = @()
-$content += (
-    "$( $text.Title )`n" +
-    "========================`n`n" +
-    $text.PSGalleryRstShield +
-    $text.PSDownloadsRstShield +
-    $text.RstExplicitLineBreak +
-    $text.AppVeyorRstShield +
-    $text.TravisCiRstShield +
-    $text.CoverallsRstShield +
-    $text.ReadTheDocsRstShield +
-    $text.RstExplicitLineBreak +
-    $text.GitterRstShield +
-    $reStructuredTextDescription +
-    "`n`nThe source code is $( $text.GitHubRst ). `n`n" +
-    $text.ArmorCompleteRstMap +
-    $text.ArmorAnywhereRstMap +
-    $text.RestfulApiRstMap +
-    $text.AppVeyorRstMap +
-    $text.TravisCiRstMap +
-    $text.PesterRstMap +
-    $text.CoverallsRstMap +
-    $text.PSGalleryRstMap +
-    $text.GitterRstMap +
-    $text.GitHubRstMap +
-    ".. toctree::`n" +
-    "   :maxdepth: 2`n" +
-    "   :hidden:`n" +
-    "   :caption: User Documentation`n"
-)
+$docsPrivatePath = Join-Path -Path $Env:CI_DOCS_PATH -ChildPath 'private' -ErrorAction 'Stop'
+$docsPublicPath = Join-Path -Path $Env:CI_DOCS_PATH -ChildPath 'public' -ErrorAction 'Stop'
+$modulePage = Join-Path -Path $docsPublicPath -ChildPath "${Env:CI_MODULE_NAME}.md" -ErrorAction 'Stop'
+$externalHelpDirectory = Join-Path -Path $Env:CI_MODULE_PATH -ChildPath 'en-US' -ErrorAction 'Stop'
 
-$fileNames = ( Get-ChildItem -Path './docs' ).Where( { $_.Name -match '^usr_\d\d_.*.rst$' } ).ForEach( { $_.Name.ToLower() } )
-foreach ( $fileName in $fileNames ) {
-    $content += "   ${fileName}"
-}
+# Update the cmdlet documentation content and add metadata for building external help files
+New-MarkdownHelp -Module $Env:CI_MODULE_NAME -Force -OutputFolder $docsPublicPath -ErrorAction 'Stop'
 
-$content += (
-    "`n`n.. toctree::`n" +
-    "   :maxdepth: 2`n" +
-    "   :hidden:`n" +
-    "   :caption: Command Documentation`n" +
-    "   "
-)
+# Update the module page content
+Update-MarkdownHelpModule -Path $docsPublicPath -RefreshModulePage -ErrorAction 'Stop'
 
-# Build the command documentation menu
-$verbs = ( Get-Command -Module $Env:CI_MODULE_NAME ).Verb.ForEach( { $_.ToLower() } ) |
-    Select-Object -Unique
-foreach ( $verb in $verbs ) {
-    $content += "   cmd_${verb}"
-}
+# Update the external help files
+New-ExternalHelp -Path $docsPublicPath -OutputPath $externalHelpDirectory -Force -ErrorAction 'Stop'
+New-ExternalHelpCab -CabFilesFolder $docsPublicPath -LandingPagePath $modulePage -OutputFolder $externalHelpDirectory -ErrorAction 'Stop'
 
-$content += ''
+# Remove the metadata from the documentation pages again
+New-MarkdownHelp -Module $Env:CI_MODULE_NAME -Force -OutputFolder $docsPublicPath -NoMetadata -ErrorAction 'Stop'
 
-# Write the index file
-$content |
-    Out-File -FilePath "${Env:CI_BUILD_PATH}/docs/index.rst" -Encoding 'utf8'
-
-Write-Host -Object "`tindex"
-
-# Build the command documentation files for each verb
-$verbs = ( Get-Command -Module $Env:CI_MODULE_NAME ).Verb |
-    Select-Object -Unique
-foreach ( $verb in $verbs ) {
-    $content = @()
-    $content += "${verb} Commands"
-    $content += '========================='
-    $content += "This page contains details on **${verb}** commands."
-    $content += ''
-
-    # Build the command documentation from the comment-based help
-    $commands = ( Get-Command -Module $Env:CI_MODULE_NAME ).Where( { $_.Verb -eq $verb } )
-    foreach ( $command in $commands ) {
-        $content += $command.Name
-        $content += '-------------------------'
-        $content += Get-Help -Name $command.name -Detailed
-        $content += ''
-    }
-
-    $verb = $verb.ToLower()
-    $content |
-        Out-File -FilePath "${Env:CI_BUILD_PATH}/docs/cmd_${verb}.rst" -Encoding 'utf8'
-
-    Write-Host -Object "`tcmd_${verb}"
+# Update the private function documentation content
+foreach ( $file in ( Get-ChildItem -Path $Env:CI_MODULE_PRIVATE_PATH -Filter '*.ps1' -ErrorAction 'Stop' ) ) {
+    . $file.FullName
+    New-MarkdownHelp -Command $file.BaseName -Force -NoMetadata -OutputFolder $docsPrivatePath -ErrorAction 'Stop'
 }
 
 Write-Host -Object ''
--- build/Install-Dependencies.ps1
+++ build/Install-Dependencies.ps1
@@ -13,7 +13,7 @@ Get-PackageProvider -Name $providerNames |
     Format-Table -AutoSize -Property 'Name', 'Version'
 
 Write-Host -Object "Installing modules:" -ForegroundColor 'Yellow'
-$moduleNames = 'Pester', 'Coveralls'
+$moduleNames = 'Pester', 'Coveralls', 'PlatyPS'
 foreach ( $moduleName in $moduleNames ) {
     if ( $Env:APPVEYOR_BUILD_WORKER_IMAGE -eq 'Visual Studio 2015' ) {
         Install-Module -Name $moduleName -Scope 'CurrentUser' -Repository 'PSGallery' -Force -Confirm:$false |
@@ -32,11 +32,14 @@ Get-Module -Name $moduleNames |
     Format-Table -AutoSize -Property 'Name', 'Version'
 
 Write-Host -Object 'NodeJS version: ' -ForegroundColor 'Yellow' -NoNewline
-& node --version
+node --version
 Write-Host -Object 'NodeJS Package Manager (npm) version: ' -ForegroundColor 'Yellow' -NoNewline
-& npm --version
+npm --version
 
 Write-Host -Object "`nInstalling npm packages: " -ForegroundColor 'Yellow'
-& npm install --global sinon@1 markdown-spellcheck 2> ( [System.IO.Path]::GetTempFileName() )
+npm install --global sinon@1 markdown-spellcheck 2> ( [System.IO.Path]::GetTempFileName() )
+
+Write-Host -Object "`nInstalling python packages: " -ForegroundColor 'Yellow'
+pip install mkdocs
 
 Write-Host -Object ''
