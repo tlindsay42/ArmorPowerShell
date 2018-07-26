2e4a0e4420c4c41e069a5f11ac44142d8f815bf3
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-07-26 01:46:45 -0500

    Update python pip dev dependency spec

--- build/build.ps1
+++ build/build.ps1
@@ -165,16 +165,16 @@ if ( $SkipDependencies -eq $false ) {
     Write-StatusUpdate -Message 'Install python development dependencies.'
     $requirementsPath = Join-Path -Path $Env:BHProjectPath -ChildPath 'requirements.txt'
     $return = $null
-    $details = python -m pip install --upgrade pip --progress-bar off |
+    $details = python -m pip install --upgrade pip |
         Out-String
     $return = $?
     if ( $Env:CI_LINUX -eq $true -or $Env:TRAVIS_OS_NAME -eq 'linux' ) {
-        $details = python -m pip install --user --requirement $requirementsPath --progress-bar off |
+        $details = python -m pip install --user --requirement $requirementsPath |
             Out-String
         $return = $?
     }
     else {
-        $details = python -m pip install --requirement $requirementsPath --progress-bar off |
+        $details = python -m pip install --requirement $requirementsPath |
             Out-String
         $return = $?
     }
