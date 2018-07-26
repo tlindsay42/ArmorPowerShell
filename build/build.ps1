aed7f05b27731883ad5f9daf0919c4e8c7553e91
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-07-26 03:00:16 -0500

    Update pip spec

--- build/build.ps1
+++ build/build.ps1
@@ -165,15 +165,18 @@ if ( $SkipDependencies -eq $false ) {
     Write-StatusUpdate -Message 'Install python development dependencies.'
     $requirementsPath = Join-Path -Path $Env:BHProjectPath -ChildPath 'requirements.txt'
     $return = $null
-    $details = python -m pip install --upgrade pip |
-        Out-String
-    $return = $?
     if ( $Env:CI_LINUX -eq $true -or $Env:TRAVIS_OS_NAME -eq 'linux' ) {
+        $details = python -m pip install --user --upgrade pip |
+            Out-String
+        $return = $?
         $details = python -m pip install --user --requirement $requirementsPath |
             Out-String
         $return = $?
     }
     else {
+        $details = python -m pip install --upgrade pip |
+            Out-String
+        $return = $?
         $details = python -m pip install --requirement $requirementsPath |
             Out-String
         $return = $?
