c5f0dcefaed546dd3336255db139e90dc93aa8bd
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-07-27 21:54:01 -0500

    Update AppVeyor author
    * https://docs.microsoft.com/en-us/nuget/reference/nuspec

--- deploy/AppVeyor.PSDeploy.ps1
+++ deploy/AppVeyor.PSDeploy.ps1
@@ -1,15 +1,16 @@
 $moduleManifest = Test-ModuleManifest -Path $Env:BHPSModuleManifest
 $moduleVersion = "$( $moduleManifest.Version )-$( Get-Metadata -Path $moduleManifest.Path -PropertyName 'Prerelease' )"
+$moduleAuthor = 'tlindsay42'
 
 Deploy 'DeveloperBuild' {
     By 'AppVeyorModule' {
         FromSource $moduleManifest.Path
         To 'AppVeyor'
         WithOptions @{
-            Author                   = 'Troy Lindsay'
+            Author                   = $moduleAuthor
             Description              = $moduleManifest.Description
             LicenseUrl               = $moduleManifest.LicenseUri
-            Owners                   = 'tlindsay42'
+            Owners                   = $moduleAuthor
             PackageName              = $moduleManifest.Name
             ProjectUrl               = $moduleManifest.ProjectUri
             Tags                     = $moduleManifest.Tags
