2bba8e013131caea98a857f57968b5ff809ac5db
Troy Lindsay <tlindsay42@users.noreply.github.com>
2019-07-09 22:01:38 -0500

    Add support for new platyPS module description template

--- build/scripts/Build-Documentation.ps1
+++ build/scripts/Build-Documentation.ps1
@@ -155,7 +155,7 @@ end {
 
     Write-StatusUpdate -Message "Update the module description in the module page: '${docsModulePageRelativePath}'."
     ( Get-Content -Path $docsModulePagePath ) -replace
-        '^{{Manually Enter Description Here}}$', "The ${ModuleName} command-line interface" |
+        '^{{(?:Manually Enter Description Here|\s?Fill in the Description\s?)}}$', "The ${ModuleName} command-line interface" |
         Set-Content -Path $docsModulePagePath -Force
 
     Write-StatusUpdate -Message 'Build the external help file.'
