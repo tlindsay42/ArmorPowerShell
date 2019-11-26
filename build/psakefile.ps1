6652d7c0ac83ea62d624e61472d0dc443bd36237
Troy Lindsay <tlindsay42@users.noreply.github.com>
2019-11-26 15:27:48 -0600

    :green_heart: Disable AppV Ubuntu [skip publish]

--- build/psakefile.ps1
+++ build/psakefile.ps1
@@ -358,7 +358,7 @@ Properties {
 
     $TEXT += @{
         Description = "This is an open source, community project that provides a powerful command-line interface for managing and monitoring your $( $TEXT.ArmorComplete ) (secure public cloud) and $( $TEXT.ArmorAnywhere ) (security as a service) environments and accounts via a PowerShell module with cmdlets that interact with the published $( $TEXT.RestfulApi ).",
-        "Every code push is built using psake on $( $TEXT.Windows ) and $( $TEXT.Ubuntu ) via $( $TEXT.AppVeyor ), as well as on $( $TEXT.macOS ) and $( $TEXT.Ubuntu ) via $( $TEXT.TravisCi ), and tested using the $( $TEXT.Pester ) test and mock framework.",
+        "Every code push is built using psake on $( $TEXT.Windows ) via $( $TEXT.AppVeyor ), as well as on $( $TEXT.macOS ) and $( $TEXT.Ubuntu ) via $( $TEXT.TravisCi ), and tested using the $( $TEXT.Pester ) test and mock framework.",
         "Code coverage scores and reports showing 100% coverage are tracked by $( $TEXT.Coveralls ).",
         "Cmdlet & private function documentation is generated programmatically via platyPS and rigorously tested to ensure accuracy.",
         "Every successful continuous integration build is continuously deployed to the $( $TEXT.AppVeyor ) NuGet project feed as a prerelease version for contributors.",
